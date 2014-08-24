8d52dbc8b39da8dd66e72eb40a7dbdd255efca44
Philip O'Toole <philip.otoole@yahoo.com>
2014-08-23 23:23:02 -0700

    Add support for pprof profiling

--- src/github.com/otoolep/rqlite/main.go
+++ src/github.com/otoolep/rqlite/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
+	"runtime/pprof"
 	"time"
 
 	"github.com/otoolep/raft"
@@ -17,12 +19,14 @@ var host string
 var port int
 var join string
 var dbfile string
+var cpuprofile string
 
 func init() {
 	flag.StringVar(&host, "h", "localhost", "hostname")
 	flag.IntVar(&port, "p", 4001, "port")
 	flag.StringVar(&join, "join", "", "host:port of leader to join")
 	flag.StringVar(&dbfile, "dbfile", "db.sqlite", "sqlite filename")
+	flag.StringVar(&cpuprofile, "cpuprofile", "", "write CPU profile to file")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [arguments] <data-path> \n", os.Args[0])
 		flag.PrintDefaults()
@@ -32,6 +36,20 @@ func init() {
 func main() {
 	flag.Parse()
 
+	// Set up profiling, if requested.
+	if cpuprofile != "" {
+		log.Printf("Profiling enabled")
+		f, err := os.Create(cpuprofile)
+		if err != nil {
+			log.Fatal("Unable to create path: %w", err)
+		}
+		defer f.Close()
+
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	// Raft requires randomness.
 	rand.Seed(time.Now().UnixNano())
 
 	// Setup commands.
@@ -48,5 +66,12 @@ func main() {
 	}
 
 	s := server.New(path, dbfile, host, port)
-	log.Fatal(s.ListenAndServe(join))
+	go func() {
+		log.Fatal(s.ListenAndServe(join))
+	}()
+
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, os.Interrupt)
+	<-terminate
+	log.Printf("rqlite server stopped")
 }
