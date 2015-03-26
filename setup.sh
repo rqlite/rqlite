e34349e5498ccfaed1c509453551857a0c137513
Mateusz Korszun <mkorszun@gmail.com>
2015-03-26 19:15:49 +0100

    Introduce vagrant env

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Dependencies
+add-apt-repository ppa:git-core/ppa -y
+echo "Updating..."
+apt-get update
+apt-get install -y curl git bison make mercurial sqlite3
+
+# Golang
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source ~/.gvm/scripts/gvm
+gvm install go1.4
+gvm use go1.4
+
+# Rqlite
+mkdir -p rqlite
+cd rqlite
+export GOPATH=$PWD
+go get github.com/otoolep/rqlite
+ln -s $GOPATH/bin/rqlite /usr/local/bin/rqlite
