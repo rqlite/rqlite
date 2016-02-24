f06cb0ec6c3a5c47c72577268bbd0b26b0cfcb03
Philip O Toole <philip.otoole@yahoo.com>
2016-02-23 20:52:43 -0500

    Final updates to package doc

--- doc.go
+++ doc.go
@@ -21,6 +21,6 @@
 // SOFTWARE.
 
 /*
-rqlite is a distributed system that provides a replicated SQLite database. rqlite is written in Go and uses Raft to achieve consensus across all the instances of the SQLite databases. rqlite ensures that every change made to the database is made to a majority of underlying SQLite files, or none at all.
+rqlite is a distributed system that provides a replicated SQLite database. rqlite uses Raft to achieve consensus across all the cluster of the SQLite databases. It ensures that every change made to the database is made to a majority of underlying SQLite files, or none at all.
 */
 package rqlite
