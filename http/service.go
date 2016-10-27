62707445849dfd0c1e4dcbb1c07c04bd9e14ab88
Philip O Toole <philip.otoole@yahoo.com>
2016-10-26 22:40:09 -0400

    Correct batch size variable name

--- http/service.go
+++ http/service.go
@@ -103,7 +103,7 @@ const (
 	PermBackup = "backup"
 
 	// LoadBatchSz is the batch size for loading a dump file.
-	LoadBatchSze = 100
+	LoadBatchSz = 100
 )
 
 func init() {
@@ -385,7 +385,7 @@ func (s *Service) handleLoad(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	n, err := s.store.Load(r.Body, LoadBatchSze)
+	n, err := s.store.Load(r.Body, LoadBatchSz)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
