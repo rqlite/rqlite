8b3665b29c5b8314ac6be92c2c15b14646941373
Philip O'Toole <philip.otoole@yahoo.com>
2017-02-19 13:01:43 -0800

    Add script for quick 3-node cluster test

---
+++ system_test/end_to_end.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+TMP_DATA=`mktemp`
+
+$GOPATH/bin/rqlited ${TMP_DATA}_1 &
+sleep 5
+$GOPATH/bin/rqlited -http localhost:4003 -raft localhost:4004 -join http://localhost:4001 ${TMP_DATA}_2 &
+sleep 5
+$GOPATH/bin/rqlited -http localhost:4005 -raft localhost:4006 -join http://localhost:4001 ${TMP_DATA}_3 &
+sleep 5
+
+wait
