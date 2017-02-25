435254547dc412ff7299b06d7253e64f3731e1af
Philip O'Toole <philip.otoole@yahoo.com>
2017-02-25 09:56:29 -0800

    Fix test logging

--- cluster/join_test.go
+++ cluster/join_test.go
@@ -76,8 +76,10 @@ func Test_DoubleJoinOKSecondNodeRedirect(t *testing.T) {
 	ts1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	}))
 	defer ts1.Close()
+	joinAddr := fmt.Sprintf("%s%s", ts1.URL, "/join")
+
 	ts2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, fmt.Sprintf("%s%s", ts1.URL, "/join"), http.StatusMovedPermanently)
+		http.Redirect(w, r, joinAddr, http.StatusMovedPermanently)
 	}))
 	defer ts2.Close()
 
@@ -85,7 +87,7 @@ func Test_DoubleJoinOKSecondNodeRedirect(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to join a single node: %s", err.Error())
 	}
-	if j != ts1.URL+"/join" {
-		t.Fatalf("node joined using wrong endpoint, exp: %s, got: %s", j, ts2.URL)
+	if j != joinAddr {
+		t.Fatalf("node joined using wrong endpoint, exp: %s, got: %s", joinAddr, j)
 	}
 }
