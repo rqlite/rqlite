7d4cd5034e2d50443def3a2cdc892cca5be3e94f
Philip O'Toole <philip.otoole@yahoo.com>
2017-04-19 10:03:34 -0700

    Make end-to-end test more explicit

--- system_test/end_to_end.sh
+++ system_test/end_to_end.sh
@@ -5,7 +5,7 @@ TMP_DATA=`mktemp`
 rm $GOPATH/bin/*
 go install ./...
 
-$GOPATH/bin/rqlited ${TMP_DATA}_1 &
+$GOPATH/bin/rqlited -http localhost:4001 -raft localhost:4002 ${TMP_DATA}_1 &
 sleep 5
 $GOPATH/bin/rqlited -http localhost:4003 -raft localhost:4004 -join http://localhost:4001 ${TMP_DATA}_2 &
 sleep 5
