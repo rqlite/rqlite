8ff3c7695b26adc86647674bafe5555cc12e9109
Philip O'Toole <philip.otoole@yahoo.com>
2017-04-22 14:44:22 -0700

    GoDoc comments

--- aws/metadata.go
+++ aws/metadata.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 )
 
+// MetadataClient is a client for fetching AWS EC2 instance metadata.
 type MetadataClient struct {
 	client *http.Client
 	URL    string
 }
 
+// NewMetadataClient returns an instance of a MetadataClient
 func NewMetadataClient() *MetadataClient {
 	return &MetadataClient{
 		client: &http.Client{},
@@ -18,10 +20,12 @@ func NewMetadataClient() *MetadataClient {
 	}
 }
 
+// LocalIPv4 returns the private IPv4 address of the instance.
 func (m *MetadataClient) LocalIPv4() (string, error) {
 	return m.get("/latest/meta-data/local-ipv4")
 }
 
+// LocalIPv4 returns the public IPv4 address of the instance.
 func (m *MetadataClient) PublicIPv4() (string, error) {
 	return m.get("/latest/meta-data/public-ipv4")
 }
