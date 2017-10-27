8bf610ca06d3b0ba87c4eadfb126be7ba7d99912
Philip O Toole <philip.otoole@yahoo.com>
2017-10-27 11:26:51 -0700

    More gRPC service implementation

--- grpc/service.go
+++ grpc/service.go
@@ -23,6 +23,12 @@ type Store interface {
 	// is true, then all queries will take place while a read transaction
 	// is held on the database.
 	Query(queries []string, timings, tx bool, lvl store.ConsistencyLevel) ([]*sql.Rows, error)
+
+	// Leader returns the Raft leader of the cluster.
+	Leader() string
+
+	// Peer returns the API peer for the given address
+	Peer(addr string) string
 }
 
 // CredentialStore is the interface credential stores must support.
@@ -36,11 +42,80 @@ type CredentialStore interface {
 
 // Service represents a gRPC service that communicates with Store.
 type Service struct {
-	grpc  *grpc.Server
-	store Store
+	grpc            *grpc.Server
+	store           Store
+	credentialStore CredentialStore
 
 	ln   net.Listener
 	addr string
 
 	logger *log.Logger
+}
+
+// New returns an instantiated service.
+func New(addr string, store Store, credentials CredentialStore) *Service {
+	s := Service{
+		grpc:            grpc.NewServer(),
+		store:           Store,
+		credentialStore: CredentialStore,
+		addr:            addr,
+		logger:          log.New(os.Stderr, "[grpc] ", log.LstdFlags),
+	}
+
+	pb.RegisterRqliteServiceServer(s.grpc, (*gprcService)(&s))
+	return &s
+}
+
+// Addr returns the address on which the service is listening.
+func (s *Service) Addr() string {
+	return s.ln.Addr().String()
+}
+
+// Open opens the service, starting it listening on the configured address.
+func (s *Service) Open() error {
+	ln, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	s.logger.Println("listening on", s.ln.Addr().String())
+
+	go func() {
+		err := s.grpc.Serve(s.ln)
+		if err != nil {
+			s.logger.Println("gRPC Serve() returned:", err.Error())
+		}
+	}()
+
+	return nil
+}
+
+// Close closes the service.
+func (s *Service) Close() error {
+	s.grpc.GracefulStop()
+	s.ln = nil
+	s.logger.Println("gRPC server stopped")
+	return nil
+}
+
+// gprcService is an unexported type, that is the same type as Service.
+//
+// Having the methods that the gRPC service requires on this type means that even though
+// the methods are exported, since the type is not, these methods are not visible outside
+// this package.
+type gprcService Service
+
+// Query implements the Query call on the gRPC service.
+func (g *gprcService) Query(c context.Context, q *pb.QueryRequest) (*pb.QueryResponse, error) {
+	return nil, nil
+}
+
+// Exec implements the Exec call on the gRPC service.
+func (g *gprcService) Exec(c context.Context, e *pb.ExecRequest) (*pb.ExecResponse, error) {
+	return nil
+}
+
+// Leader implements the Leader call on the gRPC service.
+func (g *gprcService) Leader(c context.Context) (string, error) {
+	return nil
 }
