2f705194177ffcb01808814722111bb1b61cb2b8
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-28 13:06:06 -0700

    More Exec unit testing

--- grpc/service_test.go
+++ grpc/service_test.go
@@ -54,16 +54,16 @@ func Test_ServiceLeader(t *testing.T) {
 	}
 }
 
-func Test_ExecSingle(t *testing.T) {
+func Test_Exec(t *testing.T) {
 	m := &MockStore{}
 	s := New("127.0.0.1:0", m, nil)
 	if err := s.Open(); err != nil {
 		t.Fatal("failed to open new service")
 	}
 	defer s.Close()
+	g := mustGrpcClient(s.Addr())
 
 	m.setSingleResult(34, 7, 389.38)
-	g := mustGrpcClient(s.Addr())
 	r, err := g.Exec(context.Background(),
 		&pb.ExecRequest{
 			Stmt: []string{"CREATE TABLE foo(id INTEGER PRIMARY KEY, name TEXT"},
@@ -71,11 +71,33 @@ func Test_ExecSingle(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to exec single request: %s", err.Error())
 	}
-	if len(r.GetResults()) != 1 &&
-		r.GetResults()[0].LastInsertId != 34 &&
-		r.GetResults()[0].RowsAffected != 7 &&
+	if len(r.GetResults()) != 1 ||
+		r.GetResults()[0].LastInsertId != 34 ||
+		r.GetResults()[0].RowsAffected != 7 ||
 		r.GetResults()[0].Time != 389.38 {
 		t.Fatal("incorrect result for exec single request")
+	}
+
+	m.setSingleResult(99, 1, 100.1)
+	m.setSingleResult(100, 1, 90.3)
+	r, err = g.Exec(context.Background(),
+		&pb.ExecRequest{
+			Stmt: []string{
+				`INSERT INTO foo(id, name) VALUES(1, "fiona"`,
+				`INSERT INTO foo(id, name) VALUES(1, "fiona")`,
+			},
+		})
+	if err != nil {
+		t.Fatalf("failed to exec single request: %s", err.Error())
+	}
+	if len(r.GetResults()) != 2 ||
+		r.GetResults()[0].LastInsertId != 99 ||
+		r.GetResults()[0].RowsAffected != 1 ||
+		r.GetResults()[0].Time != 100.1 ||
+		r.GetResults()[1].LastInsertId != 100 ||
+		r.GetResults()[1].RowsAffected != 1 ||
+		r.GetResults()[1].Time != 90.3 {
+		t.Fatal("incorrect result for exec multi request")
 	}
 }
 
@@ -109,7 +131,7 @@ func (m *MockStore) setSingleResult(lastInsertId, rowsAffected int64, time float
 		RowsAffected: rowsAffected,
 		Time:         time,
 	}
-	m.results = []*db.Result{r}
+	m.results = append(m.results, r)
 }
 
 type mockCredentialStore struct {
