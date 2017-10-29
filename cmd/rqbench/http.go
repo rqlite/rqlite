6e4082ad452d066d78c1b0c1cf157ee8dcc8acb3
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-29 15:24:33 -0700

    Simple INSERT-only benchmarking tool

---
+++ cmd/rqbench/http.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type HTTPTester struct {
+	client http.Client
+	url    string
+	br     *bytes.Reader
+}
+
+func NewHTTPTester(addr string) *HTTPTester {
+	return &HTTPTester{
+		client: http.Client{},
+		url:    fmt.Sprintf("http://%s:/db/execute", addr),
+	}
+}
+
+func (h *HTTPTester) Prepare(stmt string, bSz int, tx bool) error {
+	s := make([]string, bSz)
+	for i := 0; i < len(s); i++ {
+		s[i] = stmt
+	}
+
+	b, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+	h.br = bytes.NewReader(b)
+
+	if tx {
+		h.url = h.url + "?transaction"
+	}
+
+	return nil
+}
+
+func (h *HTTPTester) Once() (time.Duration, error) {
+	h.br.Seek(0, io.SeekStart)
+
+	start := time.Now()
+	resp, err := h.client.Post(h.url, "application/json", h.br)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("received %s", resp.Status)
+	}
+	dur := time.Since(start)
+
+	return dur, nil
+}
