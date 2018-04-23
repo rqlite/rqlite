5e78e3e764fe3e19da60fe0506b8294400609539
Philip O'Toole <philip.otoole@yahoo.com>
2018-04-23 09:35:21 -0400

    "peers" status key should be "nodes"

--- store/store.go
+++ store/store.go
@@ -376,6 +376,7 @@ func (s *Store) Stats() (map[string]interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+	
 	leaderID, err := s.LeaderID()
 	if err != nil {
 		return nil, err
@@ -393,7 +394,7 @@ func (s *Store) Stats() (map[string]interface{}, error) {
 		"heartbeat_timeout":  s.HeartbeatTimeout.String(),
 		"snapshot_threshold": s.SnapshotThreshold,
 		"metadata":           s.meta,
-		"peers":              nodes,
+		"nodes":              nodes,
 		"dir":                s.raftDir,
 		"sqlite3":            dbStatus,
 		"db_conf":            s.dbConf,
