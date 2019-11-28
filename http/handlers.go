ad34db2646d9385d2616148533bd6b840aedc8c2
xinyulong <xinyulong@sensetime.com>
2019-11-28 14:27:02 +0800

    Fix redirect lost url path error

--- http/handlers.go
+++ http/handlers.go
@@ -10,6 +10,30 @@ import (
 	"strings"
 )
 
+const (
+	majorActionDB     = "db"
+	majorActionJoin   = "join"
+	majorActionRemove = "remove"
+	majorActionStatus = "status"
+	majorActionDebug  = "debug"
+
+	subActionDBExecute    = "execute"
+	subActionDBQuery      = "query"
+	subActionDBBackup     = "backup"
+	subActionDBLoad       = "load"
+	subActionDBConnection = "connections"
+
+	subActionConnectionExcute = "execute"
+	subActionConnectionQuery  = "query"
+
+	subActionDebugVars  = "vars"
+	subActionDebugPprof = "pprof"
+
+	subActionDebugPprofCmdline = "cmdline"
+	subActionDebugPprofProfile = "profile"
+	subActionDebugPprofSymbol  = "symbol"
+)
+
 // Handler pattern contained here created by github.com/Merovius
 // See https://blog.merovius.de/2017/06/18/how-not-to-use-an-http-router.html
 
@@ -25,7 +49,7 @@ func (h *executeHandler) Handler(connID uint64, s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "POST" {
+		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -45,7 +69,7 @@ func (h *queryHandler) Handler(connID uint64, s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "GET" && r.Method != "POST" {
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -63,7 +87,7 @@ func (h *backupHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "GET" {
+		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -81,7 +105,7 @@ func (h *loadHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "POST" {
+		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -102,10 +126,9 @@ func (h *connectionsHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		var head string
-		head, r.URL.Path = shiftPath(r.URL.Path)
+		head := getConnectionID(r.URL.Path)
 		if head == "" {
-			if r.Method == "POST" {
+			if r.Method == http.MethodPost {
 				s.createConnection(w, r)
 
 			} else {
@@ -116,7 +139,7 @@ func (h *connectionsHandler) Handler(s *Service) http.Handler {
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusNotFound)
 			}
-			if r.Method == "DELETE" {
+			if r.Method == http.MethodDelete {
 				if err := s.deleteConnection(id); err != nil {
 					if err == ErrConnectionNotFound {
 						http.Error(w, err.Error(), http.StatusNotFound)
@@ -126,11 +149,11 @@ func (h *connectionsHandler) Handler(s *Service) http.Handler {
 				}
 				w.WriteHeader(http.StatusNoContent)
 			} else {
-				head, _ = shiftPath(r.URL.Path)
+				head = getConnectionSubaction(r.URL.Path)
 				switch head {
-				case "execute":
+				case subActionConnectionExcute:
 					h.executeH.Handler(id, s).ServeHTTP(w, r)
-				case "query":
+				case subActionConnectionQuery:
 					h.queryH.Handler(id, s).ServeHTTP(w, r)
 				default:
 					w.WriteHeader(http.StatusNotFound)
@@ -150,18 +173,17 @@ type databaseHandler struct {
 
 func (h *databaseHandler) Handler(s *Service) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var head string
-		head, r.URL.Path = shiftPath(r.URL.Path)
+		head := getSubAction(r.URL.Path)
 		switch head {
-		case "execute":
+		case subActionDBExecute:
 			h.executeH.Handler(defaultConnID, s).ServeHTTP(w, r)
-		case "query":
+		case subActionDBQuery:
 			h.queryH.Handler(defaultConnID, s).ServeHTTP(w, r)
-		case "backup":
+		case subActionDBBackup:
 			h.backupH.Handler(s).ServeHTTP(w, r)
-		case "load":
+		case subActionDBLoad:
 			h.loadH.Handler(s).ServeHTTP(w, r)
-		case "connections":
+		case subActionDBConnection:
 			h.connectionsH.Handler(s).ServeHTTP(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
@@ -178,7 +200,7 @@ func (h *joinHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "POST" {
+		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -195,7 +217,7 @@ func (h *removeHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "DELETE" {
+		if r.Method != http.MethodDelete {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -214,7 +236,7 @@ func (h *statusHandler) Handler(s *Service) http.Handler {
 			return
 		}
 
-		if r.Method != "GET" {
+		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
@@ -263,13 +285,13 @@ func (h *pprofHandler) Handler(s *Service) http.Handler {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-
-		switch r.URL.Path {
-		case "cmdline":
+		head := getDebugPprofSubaction(r.URL.Path)
+		switch head {
+		case subActionDebugPprofCmdline:
 			pprof.Cmdline(w, r)
-		case "profile":
+		case subActionDebugPprofProfile:
 			pprof.Profile(w, r)
-		case "symbol":
+		case subActionDebugPprofSymbol:
 			pprof.Symbol(w, r)
 		default:
 			pprof.Index(w, r)
@@ -284,12 +306,11 @@ type debugHandler struct {
 
 func (h *debugHandler) Handler(s *Service) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var head string
-		head, r.URL.Path = shiftPath(r.URL.Path)
+		head := getSubAction(r.URL.Path)
 		switch head {
-		case "vars":
+		case subActionDebugVars:
 			h.varsH.Handler(s).ServeHTTP(w, r)
-		case "pprof":
+		case subActionDebugPprof:
 			h.pprofH.Handler(s).ServeHTTP(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
@@ -317,18 +338,17 @@ func (h *rootHandler) Handler(s *Service) http.Handler {
 			}
 		}
 
-		var head string
-		head, r.URL.Path = shiftPath(r.URL.Path)
+		head := getMajorAction(r.URL.Path)
 		switch head {
-		case "db":
+		case majorActionDB:
 			h.dbH.Handler(s).ServeHTTP(w, r)
-		case "join":
+		case majorActionJoin:
 			h.joinH.Handler(s).ServeHTTP(w, r)
-		case "remove":
+		case majorActionRemove:
 			h.removeH.Handler(s).ServeHTTP(w, r)
-		case "status":
+		case majorActionStatus:
 			h.statusH.Handler(s).ServeHTTP(w, r)
-		case "debug":
+		case majorActionDebug:
 			h.debugH.Handler(s).ServeHTTP(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
@@ -343,4 +363,53 @@ func shiftPath(p string) (head, tail string) {
 		return p[1:], "/"
 	}
 	return p[1:i], p[i:]
+}
+
+func getMajorAction(path string) string {
+	head, _ := shiftPath(path)
+	return head
+}
+
+func getSubAction(path string) string {
+	//shift path two times, first for majory action, second for sub actions
+	_, path = shiftPath(path)
+	head, path := shiftPath(path)
+	return head
+}
+
+func getConnectionID(path string) string {
+	//shift path two times, first for majory action, second for sub actions
+	_, path = shiftPath(path)
+	subAction, path := shiftPath(path)
+	if subAction != subActionDBConnection {
+		return ""
+	}
+	connectionID, path := shiftPath(path)
+
+	return connectionID
+}
+
+func getConnectionSubaction(path string) string {
+	//shift path two times, first for majory action, second for sub actions
+	_, path = shiftPath(path)
+	subAction, path := shiftPath(path)
+	if subAction != subActionDBConnection {
+		return ""
+	}
+	_, path = shiftPath(path)
+	connectionAction, path := shiftPath(path)
+
+	return connectionAction
+}
+
+func getDebugPprofSubaction(path string) string {
+	//shift path two times, first for majory action, second for sub actions
+	_, path = shiftPath(path)
+	subAction, path := shiftPath(path)
+	if subAction != subActionDebugPprof {
+		return ""
+	}
+	action, path := shiftPath(path)
+
+	return action
 }
