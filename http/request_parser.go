c9940ca9cf4410eb928414e5691925e6399b831b
Philip O'Toole <philip.otoole@yahoo.com>
2020-09-26 21:15:49 -0400

    Add Parser for multi-format HTTP requests

---
+++ http/request_parser.go
@@ -0,0 +1,61 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/rqlite/rqlite/store"
+)
+
+var (
+	// ErrNoStatements is returned when a request is empty
+	ErrNoStatements = errors.New("no statements")
+
+	// ErrInvalidRequest is returned when a request cannot be parsed.
+	ErrInvalidRequest = errors.New("invalid request")
+)
+
+// ParseRequest generates a set of Statements for a given byte slice.
+func ParseRequest(b []byte) ([]store.Statement, error) {
+	if b == nil {
+		return nil, ErrNoStatements
+	}
+
+	simple := []string{}               // Represents a set of unparameterized queries
+	parameterized := [][]interface{}{} //Represents a set of parameterized queries
+
+	// Try simple form first.
+	err := json.Unmarshal(b, &simple)
+	if err == nil {
+		if len(simple) == 0 {
+			return nil, ErrNoStatements
+		}
+
+		stmts := make([]store.Statement, len(simple))
+		for i := range simple {
+			stmts[i].Query = simple[i]
+		}
+		return stmts, nil
+	}
+
+	// Next try parameterized form.
+	if err := json.Unmarshal(b, &parameterized); err != nil {
+		return nil, ErrInvalidRequest
+	}
+	stmts := make([]store.Statement, len(parameterized))
+
+	for i := range parameterized {
+		stmts[i].Query = parameterized[i][0].(string)
+		if len(parameterized[i]) == 1 {
+			continue
+		}
+
+		stmts[i].Parameters = make([]store.Value, len(parameterized[i])-1)
+
+		for j := range parameterized[i][1:] {
+			stmts[i].Parameters[j] = parameterized[i][j+1]
+		}
+	}
+	return stmts, nil
+
+}
---
+++ http/request_parser_test.go
@@ -0,0 +1,141 @@
+package http
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_EmptyRequest(t *testing.T) {
+	_, err := ParseRequest(nil)
+	if err != ErrNoStatements {
+		t.Fatalf("nil request did not result in correct error")
+	}
+}
+
+func Test_SingleSimpleRequest(t *testing.T) {
+	s := "SELECT * FROM FOO"
+	b := []byte(fmt.Sprintf(`["%s"]`, s))
+
+	stmts, err := ParseRequest(b)
+	if err != nil {
+		t.Fatalf("failed to parse request: %s", err.Error())
+	}
+
+	if len(stmts) != 1 {
+		t.Fatalf("incorrect number of statements returned: %d", len(stmts))
+	}
+	if stmts[0].Query != s {
+		t.Fatalf("incorrect statement parsed, exp %s, got %s", s, stmts[0].Query)
+	}
+	if stmts[0].Parameters != nil {
+		t.Fatal("statment parameters are not nil")
+	}
+}
+
+func Test_SingleSimpleInvalidRequest(t *testing.T) {
+	s := "SELECT * FROM FOO"
+	b := []byte(fmt.Sprintf(`["%s"`, s))
+
+	_, err := ParseRequest(b)
+	if err != ErrInvalidRequest {
+		t.Fatal("got unexpected error for invalid request")
+	}
+}
+
+func Test_DoubleSimpleRequest(t *testing.T) {
+	s0 := "SELECT * FROM FOO"
+	s1 := "SELECT * FROM BAR"
+	b := []byte(fmt.Sprintf(`["%s", "%s"]`, s0, s1))
+
+	stmts, err := ParseRequest(b)
+	if err != nil {
+		t.Fatalf("failed to parse request: %s", err.Error())
+	}
+
+	if len(stmts) != 2 {
+		t.Fatalf("incorrect number of statements returned: %d", len(stmts))
+	}
+	if stmts[0].Query != s0 {
+		t.Fatalf("incorrect statement parsed, exp %s, got %s", s0, stmts[0].Query)
+	}
+	if stmts[0].Parameters != nil {
+		t.Fatal("statment parameters are not nil")
+	}
+	if stmts[1].Query != s1 {
+		t.Fatalf("incorrect statement parsed, exp %s, got %s", s1, stmts[1].Query)
+	}
+	if stmts[1].Parameters != nil {
+		t.Fatal("statment parameters are not nil")
+	}
+}
+
+func Test_SingleParameterizedRequest(t *testing.T) {
+	s := "SELECT * FROM ? ?"
+	p0 := "FOO"
+	p1 := 1
+	b := []byte(fmt.Sprintf(`[["%s", "%s", %d]]`, s, p0, p1))
+
+	stmts, err := ParseRequest(b)
+	if err != nil {
+		t.Fatalf("failed to parse request: %s", err.Error())
+	}
+
+	if len(stmts) != 1 {
+		t.Fatalf("incorrect number of statements returned: %d", len(stmts))
+	}
+	if stmts[0].Query != s {
+		t.Fatalf("incorrect statement parsed, exp %s, got %s", s, stmts[0].Query)
+	}
+
+	if len(stmts[0].Parameters) != 2 {
+		t.Fatalf("incorrect number of parameters returned: %d", len(stmts[0].Parameters))
+	}
+	if stmts[0].Parameters[0] != p0 {
+		t.Fatalf("incorrect paramter, exp %s, got %s", p0, stmts[0].Parameters[0])
+	}
+	if int(stmts[0].Parameters[1].(float64)) != p1 {
+		t.Fatalf("incorrect paramter, exp %d, got %d", p1, stmts[0].Parameters[1])
+	}
+}
+
+func Test_SingleSimpleParameterizedRequest(t *testing.T) {
+	s := "SELECT * FROM ? ?"
+	b := []byte(fmt.Sprintf(`[["%s"]]`, s))
+
+	stmts, err := ParseRequest(b)
+	if err != nil {
+		t.Fatalf("failed to parse request: %s", err.Error())
+	}
+
+	if len(stmts) != 1 {
+		t.Fatalf("incorrect number of statements returned: %d", len(stmts))
+	}
+	if stmts[0].Query != s {
+		t.Fatalf("incorrect statement parsed, exp %s, got %s", s, stmts[0].Query)
+	}
+
+	if stmts[0].Parameters != nil {
+		t.Fatal("statment parameters are not nil")
+	}
+}
+
+func Test_SingleInvalidParameterizedRequest(t *testing.T) {
+	s := "SELECT * FROM ? ?"
+	p0 := "FOO"
+	p1 := 1
+	b := []byte(fmt.Sprintf(`[["%s", "%s", %d]`, s, p0, p1))
+
+	_, err := ParseRequest(b)
+	if err != ErrInvalidRequest {
+		t.Fatal("got unexpected error for invalid request")
+	}
+}
+
+func Test_MixedInvalidRequest(t *testing.T) {
+	b := []byte(`[["SELECT * FROM foo"], "SELECT * FROM bar"]`)
+
+	_, err := ParseRequest(b)
+	if err != ErrInvalidRequest {
+		t.Fatal("got unexpected error for invalid request")
+	}
+}
