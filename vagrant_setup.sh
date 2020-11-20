7d9ee1e0e0cc3193a1943d2ab7b9eca12bd334da
Philip O'Toole <philip.otoole@yahoo.com>
2020-11-20 13:14:24 -0500

    Move to go1.14

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -9,8 +9,8 @@ apt-get install -y curl git bison make mercurial
 # Go
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source ~/.gvm/scripts/gvm
-gvm install go1.13
-gvm use go1.13
+gvm install go1.14
+gvm use go1.14
 
 # rqlite
 mkdir -p rqlite
