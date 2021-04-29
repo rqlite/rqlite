0c537486369ce932e8b15c8f65e0a8c612ba2a50
Philip O'Toole <philip.otoole@yahoo.com>
2021-04-28 20:51:27 -0400

    Unit test mux with TLS transport

--- cluster/service_test.go
+++ cluster/service_test.go
@@ -3,8 +3,11 @@ package cluster
 import (
 	"crypto/tls"
 	"net"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/rqlite/rqlite/testdata/x509"
 )
 
 func Test_NewServiceOpenClose(t *testing.T) {
@@ -59,6 +62,7 @@ func Test_NewServiceSetGetNodeAPIAddr(t *testing.T) {
 
 	s.SetAPIAddr("foo")
 
+	// Test by connecting to itself.
 	addr, err := s.GetNodeAPIAddr(s.Addr())
 	if err != nil {
 		t.Fatalf("failed to get node API address: %s", err)
@@ -81,9 +85,46 @@ func Test_NewServiceSetGetNodeAPIAddr(t *testing.T) {
 	}
 }
 
+func Test_NewServiceSetGetNodeAPIAddrTLS(t *testing.T) {
+	ml := mustNewMockTLSTransport()
+	s := NewService(ml)
+	if s == nil {
+		t.Fatalf("failed to create cluster service")
+	}
+
+	if err := s.Open(); err != nil {
+		t.Fatalf("failed to open cluster service")
+	}
+
+	s.SetAPIAddr("foo")
+
+	// Test by connecting to itself.
+	addr, err := s.GetNodeAPIAddr(s.Addr())
+	if err != nil {
+		t.Fatalf("failed to get node API address: %s", err)
+	}
+	exp := "http://foo"
+	if addr != exp {
+		t.Fatalf("failed to get correct node API address, exp %s, got %s", exp, addr)
+	}
+
+	s.EnableHTTPS(true)
+	addr, err = s.GetNodeAPIAddr(s.Addr())
+	if err != nil {
+		t.Fatalf("failed to get node API address: %s", err)
+	}
+	if addr != "https://foo" {
+		t.Fatalf("failed to get correct node API address, exp %s, got %s", "https://foo", addr)
+	}
+
+	if err := s.Close(); err != nil {
+		t.Fatalf("failed to close cluster service")
+	}
+}
+
 type mockTransport struct {
-	tn        net.Listener
-	tlsConfig *tls.Config
+	tn              net.Listener
+	remoteEncrypted bool
 }
 
 func mustNewMockTransport() *mockTransport {
@@ -97,7 +138,11 @@ func mustNewMockTransport() *mockTransport {
 }
 
 func mustNewMockTLSTransport() *mockTransport {
-	// Do this next
+	tn := mustNewMockTransport()
+	return &mockTransport{
+		tn:              tls.NewListener(tn, mustCreateTLSConfig()),
+		remoteEncrypted: true,
+	}
 }
 
 func (ml *mockTransport) Accept() (c net.Conn, err error) {
@@ -112,6 +157,39 @@ func (ml *mockTransport) Close() (err error) {
 	return ml.tn.Close()
 }
 
-func (ml *mockTransport) Dial(addr string, t time.Duration) (net.Conn, error) {
-	return net.DialTimeout("tcp", addr, 5*time.Second)
+func (ml *mockTransport) Dial(addr string, timeout time.Duration) (net.Conn, error) {
+	dialer := &net.Dialer{Timeout: timeout}
+
+	var err error
+	var conn net.Conn
+	if ml.remoteEncrypted {
+		conf := &tls.Config{
+			InsecureSkipVerify: true,
+		}
+		conn, err = tls.DialWithDialer(dialer, "tcp", addr, conf)
+	} else {
+		conn, err = dialer.Dial("tcp", addr)
+	}
+
+	return conn, err
+}
+
+func mustCreateTLSConfig() *tls.Config {
+	var err error
+
+	certFile := x509.CertFile("")
+	defer os.Remove(certFile)
+	keyFile := x509.KeyFile("")
+	defer os.Remove(keyFile)
+
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	config.Certificates = make([]tls.Certificate, 1)
+	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		panic("failed to create TLS config")
+	}
+
+	return config
 }
