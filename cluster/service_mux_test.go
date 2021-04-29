0ab519f6dce89b7cf06de1144e39d8aa9e96e438
Philip O'Toole <philip.otoole@yahoo.com>
2021-04-28 20:55:48 -0400

    Test cluster service with TLS Mux

--- cluster/service_mux_test.go
+++ cluster/service_mux_test.go
@@ -3,13 +3,47 @@ package cluster
 import (
 	"fmt"
 	"net"
+	"os"
 	"testing"
 
 	"github.com/rqlite/rqlite/tcp"
+	"github.com/rqlite/rqlite/testdata/x509"
 )
 
 func Test_NewServiceSetGetNodeAPIAddrMuxed(t *testing.T) {
 	ln, mux := mustNewMux()
+	go mux.Serve()
+	tn := mux.Listen(1) // Could be any byte value.
+
+	s := NewService(tn)
+	if s == nil {
+		t.Fatalf("failed to create cluster service")
+	}
+
+	if err := s.Open(); err != nil {
+		t.Fatalf("failed to open cluster service")
+	}
+
+	s.SetAPIAddr("foo")
+
+	addr, err := s.GetNodeAPIAddr(s.Addr())
+	if err != nil {
+		t.Fatalf("failed to get node API address: %s", err)
+	}
+	if addr != "http://foo" {
+		t.Fatalf("failed to get correct node API address")
+	}
+
+	if err := ln.Close(); err != nil {
+		t.Fatalf("failed to close Mux's listener: %s", err)
+	}
+	if err := s.Close(); err != nil {
+		t.Fatalf("failed to close cluster service")
+	}
+}
+
+func Test_NewServiceSetGetNodeAPIAddrMuxedTLS(t *testing.T) {
+	ln, mux := mustNewTLSMux()
 	go mux.Serve()
 	tn := mux.Listen(1) // Could be any byte value.
 
@@ -50,6 +84,26 @@ func mustNewMux() (net.Listener, *tcp.Mux) {
 	if err != nil {
 		panic(fmt.Sprintf("failed to create mux: %s", err))
 	}
+
+	return ln, mux
+}
+
+func mustNewTLSMux() (net.Listener, *tcp.Mux) {
+	ln, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		panic("failed to create mock listener")
+	}
+
+	cert := x509.CertFile("")
+	defer os.Remove(cert)
+	key := x509.KeyFile("")
+	defer os.Remove(key)
+
+	mux, err := tcp.NewTLSMux(ln, nil, cert, key, "")
+	if err != nil {
+		panic(fmt.Sprintf("failed to create TLS mux: %s", err))
+	}
+	mux.InsecureSkipVerify = true
 
 	return ln, mux
 }
