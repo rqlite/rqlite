372e001ea13ec2f7b09fcaac51c916ef0da4aaab
Philip O'Toole <philip.otoole@yahoo.com>
2021-05-09 22:11:29 -0400

    CHANGELOG

--- cmd/version.go
+++ cmd/version.go
@@ -2,9 +2,15 @@ package cmd
 
 // These variables are populated via the Go linker.
 var (
-	Version   = "6"
-	Commit    = "unknown"
-	Branch    = "unknown"
+	// rqlite version
+	Version = "6"
+
+	// Commit this code was built at.
+	Commit = "unknown"
+
+	// Branch the code was built from.
+	Branch = "unknown"
+
+	// Timestamp of build.
 	Buildtime = "unknown"
-	Features  = []string{}
 )
