06bbc8206d7ba1d17d4a6a1cce1196ec05cb3e9e
Philip O'Toole <philip.otoole@yahoo.com>
2021-05-19 22:04:36 -0400

    Basic passthru testing of PooledTransport

--- cluster/pool.go
+++ cluster/pool.go
@@ -1,42 +1,55 @@
 package cluster
 
 import (
-	"errors"
 	"net"
 	"sync"
+	"time"
 )
 
-type Pool struct {
-	mu    sync.RWMutex
-	conns chan net.Conn
+type PoolConn struct {
+	net.Conn
+	conn net.Conn
+	pool *PooledTransport
 }
 
-type PoolConn struct {
-	net.Conn // XXXX how does this work?
-	conn     net.Conn
-	pool     *Pool
+func (pc *PoolConn) Write(b []byte) (n int, err error) {
+	return pc.conn.Write(b)
+}
+
+func (pc *PoolConn) Read(b []byte) (n int, err error) {
+	return pc.conn.Read(b)
 }
 
 func (pc *PoolConn) Close() error {
-	// XXX add back to pool
 	return pc.conn.Close()
 }
 
-func NewPool(initialCap, maxCap int) (*Pool, error) {
-	if initialCap < 0 || maxCap <= 0 || initialCap > maxCap {
-		return nil, errors.New("invalid capacity settings")
-	}
-
-	c := &Pool{
-		conns: make(chan net.Conn, maxCap),
-	}
-
-	return c, nil
+type PooledTransport struct {
+	mu    sync.RWMutex
+	tn    Transport
+	conns map[string]chan net.Conn
 }
 
-func (p *Pool) WrapConn(c net.Conn) net.Conn {
-	return &PoolConn{
-		conn: c,
-		pool: p,
+func NewPooledTransport(tn Transport) *PooledTransport {
+	c := &PooledTransport{
+		tn: tn,
 	}
+
+	return c
+}
+
+func (pt *PooledTransport) Dial(address string, timeout time.Duration) (net.Conn, error) {
+	return pt.tn.Dial(address, timeout)
+}
+
+func (pt *PooledTransport) Accept() (net.Conn, error) {
+	return pt.tn.Accept()
+}
+
+func (pt *PooledTransport) Close() error {
+	return pt.tn.Close()
+}
+
+func (pt *PooledTransport) Addr() net.Addr {
+	return pt.tn.Addr()
 }
--- cluster/service_test.go
+++ cluster/service_test.go
@@ -122,12 +122,48 @@ func Test_NewServiceSetGetNodeAPIAddrTLS(t *testing.T) {
 	}
 }
 
+func Test_NewServiceSetGetNodeAPIAddrPooled(t *testing.T) {
+	ml := mustNewMockPooledTransport()
+	s := New(ml)
+	if s == nil {
+		t.Fatalf("failed to create cluster service")
+	}
+
+	if err := s.Open(); err != nil {
+		t.Fatalf("failed to open cluster service")
+	}
+
+	s.SetAPIAddr("foo")
+
+	// Test by connecting to itself.
+	addr, err := s.GetNodeAPIAddr(s.Addr())
+	if err != nil {
+		t.Fatalf("failed to get node API address: %s", err)
+	}
+	if addr != "http://foo" {
+		t.Fatalf("failed to get correct node API address, exp %s, got %s", "http://foo", addr)
+	}
+
+	s.EnableHTTPS(true)
+	addr, err = s.GetNodeAPIAddr(s.Addr())
+	if err != nil {
+		t.Fatalf("failed to get node API address: %s", err)
+	}
+	if addr != "https://foo" {
+		t.Fatalf("failed to get correct node API address, exp %s, got %s", "https://foo", addr)
+	}
+
+	if err := s.Close(); err != nil {
+		t.Fatalf("failed to close cluster service")
+	}
+}
+
 type mockTransport struct {
 	tn              net.Listener
 	remoteEncrypted bool
 }
 
-func mustNewMockTransport() *mockTransport {
+func mustNewMockTransport() Transport {
 	tn, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		panic("failed to create mock listener")
@@ -137,12 +173,16 @@ func mustNewMockTransport() *mockTransport {
 	}
 }
 
-func mustNewMockTLSTransport() *mockTransport {
+func mustNewMockTLSTransport() Transport {
 	tn := mustNewMockTransport()
 	return &mockTransport{
 		tn:              tls.NewListener(tn, mustCreateTLSConfig()),
 		remoteEncrypted: true,
 	}
+}
+
+func mustNewMockPooledTransport() Transport {
+	return NewPooledTransport(mustNewMockTransport())
 }
 
 func (ml *mockTransport) Accept() (c net.Conn, err error) {
