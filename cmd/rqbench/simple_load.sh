661be97ca7788807ad3fc14da096de856bd4bbed
Philip O'Toole <philip.otoole@yahoo.com>
2021-08-23 16:25:07 -0400

    Update simple load test

--- cmd/rqbench/simple_load.sh
+++ cmd/rqbench/simple_load.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-$RQBENCH -o 'CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT, surname TEXT)' -m 100 -n 1500000 'INSERT INTO foo(name) VALUES("fiona")' &
-$RQBENCH -o 'CREATE TABLE bar (id INTEGER NOT NULL PRIMARY KEY, name TEXT, surname TEXT)' -m 100 -n 1500000 'INSERT INTO bar(name, surname) VALUES("fiona", "OTOOLE")' &
-$RQBENCH -o 'CREATE TABLE qux (id INTEGER NOT NULL PRIMARY KEY, name TEXT)' -m 100 -n 1500000 'INSERT INTO qux(name) VALUES("fionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafiona")' &
+EXECUTE_HOST=localhost:4001
 
-sleep 60
-$RQBENCH -p "/db/query" -n 15000000 -m 1000 "SELECT COUNT(*) FROM foo" &
-sleep 60 
-$RQBENCH -p "/db/query" -n 15000000 -m 1000 "SELECT COUNT(*) FROM bar" &
-sleep 60
-$RQBENCH -p "/db/query" -n 15000000 -m 1000 "SELECT * FROM qux LIMIT 10" &
+$RQBENCH -o 'CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT, surname TEXT)' -m 100 -n 1500000 -a $EXECUTE_HOST 'INSERT INTO foo(name) VALUES("fiona")' &
+$RQBENCH -o 'CREATE TABLE bar (id INTEGER NOT NULL PRIMARY KEY, name TEXT, surname TEXT)' -m 100 -n 1500000 -a $EXECUTE_HOST 'INSERT INTO bar(name, surname) VALUES("fiona", "OTOOLE")' &
+$RQBENCH -o 'CREATE TABLE qux (id INTEGER NOT NULL PRIMARY KEY, name TEXT)' -m 100 -n 1500000 -a $EXECUTE_HOST 'INSERT INTO qux(name) VALUES("fionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafionafiona")' &
+
+$RQBENCH -p "/db/query" -n 150000000 -m 1000 "SELECT COUNT(*) FROM foo" &
+$RQBENCH -p "/db/query" -n 150000000 -m 1000 "SELECT COUNT(*) FROM bar" &
+$RQBENCH -p "/db/query" -n 150000000 -m 1000 "SELECT * FROM qux LIMIT 10" &
 
 wait
