81a234abd00aae78fc3e37e918c1d3299f537fdc
Philip O'Toole <philip.otoole@yahoo.com>
2021-08-25 09:26:03 -0400

    Command line option for SQLite on-disk file path

--- cmd/rqlited/main.go
+++ cmd/rqlited/main.go
@@ -62,6 +62,7 @@ var discoID string
 var expvar bool
 var pprofEnabled bool
 var onDisk bool
+var onDiskPath string
 var fkConstraints bool
 var raftLogLevel string
 var raftNonVoter bool
@@ -110,6 +111,7 @@ func init() {
 	flag.BoolVar(&expvar, "expvar", true, "Serve expvar data on HTTP server")
 	flag.BoolVar(&pprofEnabled, "pprof", true, "Serve pprof data on HTTP server")
 	flag.BoolVar(&onDisk, "on-disk", false, "Use an on-disk SQLite database")
+	flag.StringVar(&onDiskPath, "on-disk-path", "", "Specify path for SQLite on-disk database file. If not set, use file in data directory")
 	flag.BoolVar(&fkConstraints, "fk", false, "Enable SQLite foreign key constraints")
 	flag.BoolVar(&showVersion, "version", false, "Show version information and exit")
 	flag.BoolVar(&raftNonVoter, "raft-non-voter", false, "Configure as non-voting node")
