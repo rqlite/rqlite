da8d4fcbe95643efe7ffe158d0ebe0fa7783c85f
Philip O'Toole <philip.otoole@yahoo.com>
2022-05-09 21:26:50 -0400

    Unit test that pre-existing data is gone

--- store/store_test.go
+++ store/store_test.go
@@ -760,15 +760,41 @@ func Test_SingleNodeLoadBinary(t *testing.T) {
 		t.Fatalf("Error waiting for leader: %s", err)
 	}
 
-	err := s.Load(loadRequestFromFile(filepath.Join("testdata", "load.sqlite")))
+	// Load a dataset, to check it's erased by the SQLite file load.
+	dump := `PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE bar (id integer not null primary key, name text);
+INSERT INTO "bar" VALUES(1,'declan');
+COMMIT;
+`
+	_, err := s.Execute(executeRequestFromString(dump, false, false))
+	if err != nil {
+		t.Fatalf("failed to load simple dump: %s", err.Error())
+	}
+
+	// Check that data were loaded correctly.
+	qr := queryRequestFromString("SELECT * FROM bar", false, true)
+	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
+	r, err := s.Query(qr)
+	if err != nil {
+		t.Fatalf("failed to query single node: %s", err.Error())
+	}
+	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+	if exp, got := `[[1,"declan"]]`, asJSON(r[0].Values); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+
+	err = s.Load(loadRequestFromFile(filepath.Join("testdata", "load.sqlite")))
 	if err != nil {
 		t.Fatalf("failed to load SQLite file: %s", err.Error())
 	}
 
 	// Check that data were loaded correctly.
-	qr := queryRequestFromString("SELECT * FROM foo WHERE id=2", false, true)
+	qr = queryRequestFromString("SELECT * FROM foo WHERE id=2", false, true)
 	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
-	r, err := s.Query(qr)
+	r, err = s.Query(qr)
 	if err != nil {
 		t.Fatalf("failed to query single node: %s", err.Error())
 	}
@@ -788,6 +814,17 @@ func Test_SingleNodeLoadBinary(t *testing.T) {
 		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
 	}
 	if exp, got := `[[3]]`, asJSON(r[0].Values); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+
+	// Check pre-existing data is gone.
+	qr = queryRequestFromString("SELECT * FROM bar", false, true)
+	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
+	r, err = s.Query(qr)
+	if err != nil {
+		t.Fatalf("failed to query single node: %s", err.Error())
+	}
+	if exp, got := `{"error":"no such table: bar"}`, asJSON(r[0]); exp != got {
 		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
 	}
 }
