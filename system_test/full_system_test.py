eb5519eb13c6c6d957ef17e2a56413e4ef6deb7d
Philip O'Toole <philip.otoole@yahoo.com>
2022-10-21 08:34:59 -0400

    Correct node ID in test

--- system_test/full_system_test.py
+++ system_test/full_system_test.py
@@ -970,7 +970,7 @@ class TestBootstrapping(unittest.TestCase):
     self.assertEqual(n0.wait_for_leader(), n2.wait_for_leader())
 
     # Ensure a 4th node can join later, with same launch params.
-    n3 = Node(RQLITED_PATH, '1', boostrap_expect=3)
+    n3 = Node(RQLITED_PATH, '4', boostrap_expect=3)
     n3.start(join=','.join([n0.APIProtoAddr(), n1.APIProtoAddr(), n2.APIProtoAddr()]))
 
     n3.wait_for_leader()
