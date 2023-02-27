c0c9d0e1b9f924013a7320448d48d20d2196b092
Philip O'Toole <philip.otoole@yahoo.com>
2023-02-27 11:00:11 -0500

    Script to generate root-signed certs

---
+++ testdata/x509/scripts/prep-ca-signed-certs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+CACONF=`mktemp`
+TMPDIR=`mktemp -d`
+
+# Generate a private key for the CA
+openssl genrsa -out $TMPDIR/root.key.pem 4096
+
+# Generate cert from the root key
+# You can review the cert afterwards via `openssl x509 -in root.cert.pem -text`
+openssl req -key $TMPDIR/root.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out $TMPDIR/root.cert.pem  -subj "/C=US/ST=Pennsylvania/L=Mars/O=rqlite /OU=IT Department/CN=ca.rqlite.io"
+
+# Generate key for a client, and a corresponding certificate signing request (CSR).
+openssl req -new -newkey rsa:1024 -nodes -keyout $TMPDIR/client.key.pem -out $TMPDIR/client.csr.pem -subj "/C=US/ST=Pennsylvania/L=Mars/O=rqlite /OU=IT Department/CN=client.rqlite.io"
+
+# Prep the "certificate authority" to process the CSR.
+echo "[ ca ]
+default_ca = ca_default
+[ ca_default ]
+certs = $TMPDIR
+new_certs_dir = $TMPDIR/ca.db.certs
+database = $TMPDIR/ca.db.index
+serial = $TMPDIR/ca.db.serial
+RANDFILE = $TMPDIR/ca.db.rand
+certificate = $TMPDIR/root.cert.pem
+private_key = $TMPDIR/root.key.pem
+default_days = 7300
+default_crl_days = 30
+default_md = md5
+preserve = no
+policy = generic_policy
+[ generic_policy ]
+countryName = optional
+stateOrProvinceName = optional
+localityName = optional
+organizationName = optional
+organizationalUnitName = optional
+commonName = optional
+emailAddress = optional
+">$CACONF
+
+mkdir $TMPDIR/ca.db.certs
+touch $TMPDIR/ca.db.index
+echo "1234" > $TMPDIR/ca.db.serial
+
+# Process the CSR
+openssl ca -config $CACONF -batch -out $TMPDIR/client.cert.pem -infiles $TMPDIR/client.csr.pem
+
+echo "CA root private key: $TMPDIR/root.key.pem"
+echo "CA root public cert: $TMPDIR/root.cert.pem"
+echo "Client private key: $TMPDIR/client.key.pem"
+echo "Client cert signed by root CA: $TMPDIR/client.cert.pem"
+
