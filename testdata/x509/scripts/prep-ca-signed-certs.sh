2a933ff36f4502976b950a107ce8cb9a0a6f0814
Philip O'Toole <philip.otoole@yahoo.com>
2023-02-27 21:42:18 -0500

    Add server cert generation to script

--- testdata/x509/scripts/prep-ca-signed-certs.sh
+++ testdata/x509/scripts/prep-ca-signed-certs.sh
@@ -10,7 +10,12 @@ openssl genrsa -out $TMPDIR/root.key.pem 4096
 # You can review the cert afterwards via `openssl x509 -in root.cert.pem -text`
 openssl req -key $TMPDIR/root.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out $TMPDIR/root.cert.pem  -subj "/C=US/ST=Pennsylvania/L=Mars/O=rqlite /OU=IT Department/CN=ca.rqlite.io"
 
+# Generate key for a server, and a corresponding certificate signing request (CSR).
+# Review the CSR via `openssl req -in server.csr.pem -text -noout`
+openssl req -new -newkey rsa:1024 -nodes -keyout $TMPDIR/server.key.pem -out $TMPDIR/server.csr.pem -subj "/C=US/ST=Pennsylvania/L=Mars/O=rqlite /OU=IT Department/CN=server.rqlite.io"
+
 # Generate key for a client, and a corresponding certificate signing request (CSR).
+# Review the CSR via `openssl req -in client.csr.pem -text -noout`
 openssl req -new -newkey rsa:1024 -nodes -keyout $TMPDIR/client.key.pem -out $TMPDIR/client.csr.pem -subj "/C=US/ST=Pennsylvania/L=Mars/O=rqlite /OU=IT Department/CN=client.rqlite.io"
 
 # Prep the "certificate authority" to process the CSR.
@@ -48,6 +53,8 @@ openssl ca -config $CACONF -batch -out $TMPDIR/client.cert.pem -infiles $TMPDIR/
 
 echo "CA root private key: $TMPDIR/root.key.pem"
 echo "CA root public cert: $TMPDIR/root.cert.pem"
+echo "Server private key: $TMPDIR/server.key.pem"
+echo "Server cert signed by root CA: $TMPDIR/server.cert.pem"
 echo "Client private key: $TMPDIR/client.key.pem"
 echo "Client cert signed by root CA: $TMPDIR/client.cert.pem"
 
