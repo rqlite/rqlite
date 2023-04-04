93b33085025009a04ded1c1d5888eeada3f78501
Philip O'Toole <philip.otoole@yahoo.com>
2023-04-03 20:42:35 -0400

    Skip any files that disappear during walking

--- store/store.go
+++ store/store.go
@@ -1886,6 +1886,11 @@ func dirSize(path string) (int64, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
+			// If the file doesn't exist, we can ignore it. Snapshot files might
+			// disappear during walking.
+			if os.IsNotExist(err) {
+				return nil
+			}
 			return err
 		}
 		if !info.IsDir() {
