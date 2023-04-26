c992f359eeae02ea85a66070e2f4dd8d0240d15c
Philip O'Toole <philip.otoole@yahoo.com>
2023-04-25 22:54:52 -0400

    Add stats and metrics to Uploader

--- upload/uploader.go
+++ upload/uploader.go
@@ -10,11 +10,13 @@ import (
 	"time"
 )
 
+// StorageClient is an interface for uploading data to a storage service.
 type StorageClient interface {
 	Upload(ctx context.Context, reader io.Reader) error
 	fmt.Stringer
 }
 
+// DataProvider is an interface for providing data to be uploaded.
 type DataProvider interface {
 	Provide() (io.Reader, error)
 }
@@ -43,14 +45,18 @@ func ResetStats() {
 	stats.Add(lastUploadBytes, 0)
 }
 
+// Uploader is a service that periodically uploads data to a storage service.
 type Uploader struct {
 	storageClient StorageClient
 	dataProvider  DataProvider
 	interval      time.Duration
 
-	logger *log.Logger
+	logger             *log.Logger
+	lastUploadTime     time.Time
+	lastUploadDuration time.Duration
 }
 
+// NewUploader creates a new Uploader service.
 func NewUploader(storageClient StorageClient, dataProvider DataProvider, interval time.Duration) *Uploader {
 	return &Uploader{
 		storageClient: storageClient,
@@ -60,7 +66,8 @@ func NewUploader(storageClient StorageClient, dataProvider DataProvider, interva
 	}
 }
 
-func (u *Uploader) Start(ctx context.Context, bucket, path string) {
+// Start starts the Uploader service.
+func (u *Uploader) Start(ctx context.Context) {
 	ticker := time.NewTicker(u.interval)
 	defer ticker.Stop()
 
@@ -76,12 +83,25 @@ func (u *Uploader) Start(ctx context.Context, bucket, path string) {
 	}
 }
 
+// Stats returns the stats for the Uploader service.
+func (u *Uploader) Stats() (map[string]interface{}, error) {
+	status := map[string]interface{}{
+		"upload_destination":   u.storageClient.String(),
+		"upload_interval":      u.interval.String(),
+		"last_upload_time":     u.lastUploadTime.Format(time.RFC3339),
+		"last_upload_duration": u.lastUploadDuration.String(),
+	}
+	return status, nil
+}
+
 func (u *Uploader) upload(ctx context.Context) error {
 	reader, err := u.dataProvider.Provide()
 	if err != nil {
 		return err
 	}
+
 	cr := &countingReader{reader: reader}
+	startTime := time.Now()
 	err = u.storageClient.Upload(context.Background(), cr)
 	if err != nil {
 		stats.Add(numUploadsFail, 1)
@@ -89,6 +109,8 @@ func (u *Uploader) upload(ctx context.Context) error {
 		stats.Add(numUploadsOKKey, 1)
 		stats.Add(totalUploadBytes, cr.count)
 		stats.Get(lastUploadBytes).(*expvar.Int).Set(cr.count)
+		u.lastUploadTime = time.Now()
+		u.lastUploadDuration = time.Since(startTime)
 	}
 	return err
 }
