102869eb6d2bee5a15256482985f79e8b8fdee9b
Philip O'Toole <philip.otoole@yahoo.com>
2023-04-26 21:35:30 -0400

    Unit test Provide() on the Store

--- store/store_test.go
+++ store/store_test.go
@@ -3,6 +3,7 @@ package store
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"net"
@@ -861,6 +862,120 @@ COMMIT;
 	}
 	if exp, got := `{"error":"no such table: bar"}`, asJSON(r[0]); exp != got {
 		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+}
+
+func Test_SingleNodeProvide(t *testing.T) {
+	for _, inmem := range []bool{
+		true,
+		false,
+	} {
+		func() {
+			s0, ln := mustNewStore(t, inmem)
+			defer ln.Close()
+
+			if err := s0.Open(); err != nil {
+				t.Fatalf("failed to open single-node store: %s", err.Error())
+			}
+			if err := s0.Bootstrap(NewServer(s0.ID(), s0.Addr(), true)); err != nil {
+				t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
+			}
+			defer s0.Close(true)
+			if _, err := s0.WaitForLeader(10 * time.Second); err != nil {
+				t.Fatalf("Error waiting for leader: %s", err)
+			}
+
+			er := executeRequestFromStrings([]string{
+				`CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`,
+				`INSERT INTO foo(id, name) VALUES(1, "fiona")`,
+			}, false, false)
+			_, err := s0.Execute(er)
+			if err != nil {
+				t.Fatalf("failed to execute on single node: %s", err.Error())
+			}
+			qr := queryRequestFromString("SELECT * FROM foo", false, false)
+			qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_NONE
+			r, err := s0.Query(qr)
+			if err != nil {
+				t.Fatalf("failed to query leader node: %s", err.Error())
+			}
+			if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
+				t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+			}
+			if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
+				t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+			}
+
+			rc, err := s0.Provide()
+			if err != nil {
+				t.Fatalf("store failed to provide: %s", err.Error())
+			}
+
+			f, err := os.CreateTemp("", "rqlite-store-test")
+			if err != nil {
+				t.Fatalf("failed to create temp file: %s", err.Error())
+			}
+			defer os.Remove(f.Name())
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				t.Fatalf("failed to copy data from store: %s", err.Error())
+			}
+
+			// Load the provided data into a new store and check it.
+			s1, ln := mustNewStore(t, true)
+			defer ln.Close()
+
+			if err := s1.Open(); err != nil {
+				t.Fatalf("failed to open single-node store: %s", err.Error())
+			}
+			if err := s1.Bootstrap(NewServer(s1.ID(), s1.Addr(), true)); err != nil {
+				t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
+			}
+			defer s1.Close(true)
+			if _, err := s1.WaitForLeader(10 * time.Second); err != nil {
+				t.Fatalf("Error waiting for leader: %s", err)
+			}
+
+			err = s1.Load(loadRequestFromFile(f.Name()))
+			if err != nil {
+				t.Fatalf("failed to load provided SQLite data: %s", err.Error())
+			}
+			qr = queryRequestFromString("SELECT * FROM foo", false, false)
+			qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
+			r, err = s1.Query(qr)
+			if err != nil {
+				t.Fatalf("failed to query leader node: %s", err.Error())
+			}
+			if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
+				t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+			}
+			if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
+				t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+			}
+		}()
+	}
+}
+
+func Test_SingleNodeInMemProvideNoData(t *testing.T) {
+	s, ln := mustNewStore(t, true)
+	defer ln.Close()
+
+	if err := s.Open(); err != nil {
+		t.Fatalf("failed to open single-node store: %s", err.Error())
+	}
+	if err := s.Bootstrap(NewServer(s.ID(), s.Addr(), true)); err != nil {
+		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
+	}
+	defer s.Close(true)
+	if _, err := s.WaitForLeader(10 * time.Second); err != nil {
+		t.Fatalf("Error waiting for leader: %s", err)
+	}
+
+	_, err := s.Provide()
+	if err != nil {
+		t.Fatalf("store failed to provide: %s", err.Error())
 	}
 }
 
