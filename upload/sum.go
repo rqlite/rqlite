320aad1bafe41061b3d4f8a945aebaf9538e6bac
Philip O'Toole <philip.otoole@yahoo.com>
2023-04-28 15:52:41 -0400

    GoDoc

--- upload/sum.go
+++ upload/sum.go
@@ -8,16 +8,20 @@ import (
 	"os"
 )
 
+// SHA256Sum is a SHA256 hash.
 type SHA256Sum []byte
 
+// String returns the hex-encoded string representation of the SHA256Sum.
 func (s SHA256Sum) String() string {
 	return hex.EncodeToString(s)
 }
 
+// Equals returns true if the SHA256Sum is equal to the other.
 func (s SHA256Sum) Equals(other SHA256Sum) bool {
 	return bytes.Equal(s, other)
 }
 
+// FileSHA256 returns the SHA256 hash of the file at the given path.
 func FileSHA256(filePath string) (SHA256Sum, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
