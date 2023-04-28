7584145c63f0341f621f21fd85c6c34259f1361d
Philip O'Toole <philip.otoole@yahoo.com>
2023-04-27 20:34:40 -0400

    End-to-end S3 backup test looks good

--- system_test/e2e/auto_backup.py
+++ system_test/e2e/auto_backup.py
@@ -18,45 +18,48 @@ from s3 import download_s3_object, delete_s3_object
 RQLITED_PATH = os.environ['RQLITED_PATH']
 
 class TestAutoBackupS3(unittest.TestCase):
-  @unittest.skipIf(not env_present('RQLITE_S3_ACCESS_KEY'), "S3 credentials not available")
+  @unittest.skipUnless(env_present('RQLITE_S3_ACCESS_KEY'), "S3 credentials not available")
   def test(self):
     '''Test that automatic backups to AWS S3 work'''
-    access_key_id = os.environ['RQLITE_S3_ACCESS_KEY']
-    secret_access_key_id = os.environ['RQLITE_S3_SECRET_ACCESS_KEY']
+    self.node = None
+    self.cfg = None
+    self.path = None
+    self.backup_file = None
+
+    self.access_key_id = os.environ['RQLITE_S3_ACCESS_KEY']
+    self.secret_access_key_id = os.environ['RQLITE_S3_SECRET_ACCESS_KEY']
 
     # Create the auto-backup config file
-    path = random_string(32)
+    self.path = random_string(32)
     auto_backup_cfg = {
       "version": 1,
       "type": "s3",
       "interval": "1s",
       "sub" : {
-         "access_key_id": access_key_id,
-         "secret_access_key": secret_access_key_id,
+         "access_key_id": self.access_key_id,
+         "secret_access_key": self.secret_access_key_id,
          "region": "us-west-2",
          "bucket": "rqlite-testing-circleci",
-         "path": path
+         "path": self.path
       }
     }
-    cfg = write_random_file(json.dumps(auto_backup_cfg))
+    self.cfg = write_random_file(json.dumps(auto_backup_cfg))
 
     # Create a node, enable automatic backups, and start it. Then
     # create a table and insert a row. Wait for a backup to happen.
-    node = Node(RQLITED_PATH, '0', auto_backup=cfg)
-    node.start()
-    node.wait_for_leader()
-    node.execute('CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)')
-    node.execute('INSERT INTO foo(name) VALUES("fiona")')
-    node.wait_for_all_fsm()
-    time.sleep(10)
-
-    deprovision_node(node)
-    os.remove(cfg)
+    self.node = Node(RQLITED_PATH, '0', auto_backup=self.cfg)
+    self.node.start()
+    self.node.wait_for_leader()
+    self.node.execute('CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)')
+    self.node.execute('INSERT INTO foo(name) VALUES("fiona")')
+    self.node.wait_for_all_fsm()
+    time.sleep(5)
 
     # Download the backup file from S3 and check it.
-    backupData = download_s3_object(access_key_id, secret_access_key_id, 'rqlite-testing-circleci', path)
-    backupFile = write_random_file(backupData, mode='wb')
-    conn = sqlite3.connect(backupFile)
+    backup_data = download_s3_object(self.access_key_id, self.secret_access_key_id,
+                                     'rqlite-testing-circleci', self.path)
+    self.backup_file = write_random_file(backup_data, mode='wb')
+    conn = sqlite3.connect(self.backup_file)
     c = conn.cursor()
     c.execute('SELECT * FROM foo')
     rows = c.fetchall()
@@ -64,9 +67,15 @@ class TestAutoBackupS3(unittest.TestCase):
     self.assertEqual(rows[0][1], 'fiona')
     conn.close()
 
-    # Remove the backup file and S3 object
-    os.remove(backupFile)
-    delete_s3_object(access_key_id, secret_access_key_id, 'rqlite-testing-circleci', path)
+  def tearDown(self):
+    if self.node is not None:
+      deprovision_node(self.node)
+    if self.cfg is not None:
+      os.remove(self.cfg)
+    if self.backup_file is not None:
+      os.remove(self.backup_file)
+    delete_s3_object(self.access_key_id, self.secret_access_key_id,
+                     'rqlite-testing-circleci', self.path)
 
 
 if __name__ == "__main__":
