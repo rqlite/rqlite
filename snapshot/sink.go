5d34f74810de1c0139188ab43fcfcabe3a2aadf6
Philip O'Toole <philip.otoole@yahoo.com>
2023-08-14 23:17:10 -0400

    More generational Sink and Store work

--- snapshot/sink.go
+++ snapshot/sink.go
@@ -1,33 +1,52 @@
 package snapshot
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 
+	"github.com/rqlite/rqlite/db"
 	"google.golang.org/protobuf/proto"
 )
 
+const (
+	baseSqliteFile    = "base-sqlite.db"
+	baseSqliteWALFile = "base-sqlite.db-wal"
+	snapWALFile       = "wal"
+	tmpSuffix         = ".tmp"
+	metaFileName      = "meta.json"
+)
+
+// Sink is a sink for a snapshot.
 type Sink struct {
 	str *Store
 	dir string // Directory for the Snapshot
 
 	walFilePath string
-
-	dataFD *os.File // File the snapshot data is written to
+	dataFD      *os.File // File the snapshot data is written to
+	meta        *Meta
 
 	logger *log.Logger
 	closed bool
 }
 
+// NewSink creates a new Sink object, ready for writing a snapshot.
+// The directory that is passed in is the directory that the snapshot
+// should be stored in, assuming that the snapshot is incremental. If
+// a full snapshot is written to the sink, then the Sink needs to create
+// a new generation directory and install the Snapshot there.
 func NewSink(str *Store, dir string, meta *Meta) *Sink {
 	tDir := tmpName(dir)
 	return &Sink{
 		str:         str,
 		dir:         tDir,
 		walFilePath: filepath.Join(tDir, snapWALFile),
+		meta:        meta,
 		logger:      log.New(os.Stderr, "[snapshot-sink] ", log.LstdFlags),
 	}
 }
@@ -70,7 +89,7 @@ func (s *Sink) Close() error {
 
 // stub out ID() and Cancel() on SnapshotSink
 func (s *Sink) ID() string {
-	return ""
+	return s.meta.ID
 }
 
 func (s *Sink) Cancel() error {
@@ -85,56 +104,179 @@ func (s *Sink) processSnapshotData() error {
 	buf := make([]byte, sizeofHeader)
 	_, err := io.ReadFull(s.dataFD, buf)
 	if err != nil {
-		return fmt.Errorf("unexpected error reading header %v", err)
+		return fmt.Errorf("error reading header %v", err)
 	}
 	h, err := DecodeHeader(buf)
 	if err != nil {
-		return fmt.Errorf("unexpected error decoding header %v", err)
+		return fmt.Errorf("error decoding header %v", err)
 	}
 
 	buf = make([]byte, h.SnapshotHeaderSize)
 	_, err = io.ReadFull(s.dataFD, buf)
 	if err != nil {
-		return fmt.Errorf("unexpected error reading snapshot header %v", err)
+		return fmt.Errorf("error reading snapshot header %v", err)
 	}
 	fsmSnap := &FSMSnapshot{}
 	err = proto.Unmarshal(buf, fsmSnap)
 	if err != nil {
-		return fmt.Errorf("unexpected error unmarshaling FSM snapshot: %v", err)
+		return fmt.Errorf("error unmarshaling FSM snapshot: %v", err)
 	}
 
 	// Incremental snapshot?
 	if incSnap := fsmSnap.GetIncrementalSnapshot(); incSnap != nil {
-		s.logger.Printf("processing incremental snapshot")
-		return os.WriteFile(s.walFilePath, incSnap.Data, 0644)
+		if err := s.processIncrementalSnapshot(incSnap); err != nil {
+			return err
+		}
+	} else {
+		// Turns out it's a full snapshot.
+		fullSnap := fsmSnap.GetFullSnapshot()
+		if fullSnap == nil {
+			return fmt.Errorf("got nil FullSnapshot")
+		}
+		if err := s.processFullSnapshot(fullSnap); err != nil {
+			return err
+		}
 	}
 
-	// Process a full snapshot. We need to write the WAL data to the
-	// snapshot directory, and the base SQLite data to the parent directory.
-
-	// Let's just easy path for now, where we don't worry about resetting the store in response
-	// to a FULL snapshot. Just assume it's the first. Won't run for every long though, I have
-	// tests with missing logs.
-
-	// Write a marker file in the parent directory to indicate that we're restoring a full snapshot.
-	if err := os.WriteFile(filepath.Join(s.dir, "RESTORING-<SNAP ID>"), nil, 0644); err != nil {
+	dstDir, err := moveFromTmp(s.dir)
+	if err != nil {
+		s.logger.Printf("failed to move snapshot directory into place: %s", err)
 		return err
 	}
 
-	// Not sure if I need marker file. The following could happen:
-	// - if there is a .tmp snapshot directory left, it will be cleaned up
-	// - this would leave a SQLite directory (and some WAL files) in the directory without any snapshot? But what about old snapshots? that might given the impression
-	// - that the SQLite is OK. This indicates older snapshot directories should be deleted if they exist. They are probably useless if a full snapshot is getting
-	// - written to here, right? This implies RESTORING might be useful.
-	// SIMPLY REMOVING EXISTING SQLITE file means that the node couldn't start up properly, since restart requires a snapshot
-	// and whatever logs are available in the Raft log.
+	// Sync parent directory to ensure snapshot is visible, but it's only
+	// needed on *nix style file systems.
+	if runtime.GOOS != "windows" {
+		if err := syncDir(parentDir(s.dir)); err != nil {
+			s.logger.Printf("failed syncing parent directory: %s", err)
+			return err
+		}
+	}
 
-	// Installing a full snapshot can't result in data loss, bad.
-	// So how about renaming any existing SQLite file to the name of the latest snapshot directory?
-	// And then on restart if RESTORING- exists, then remove any base SQLite file, and restore the
-	// named SQLite to its rightful place. Delete any snapshot dirs newer than name.
-	// If RESTORING- is not there, but there is a SQLite named after a snapshot, then it must be named
-	// after the second newest one. We know we can then delete that SQLite file, and any snapshots
-	// of that name (and older).
+	s.logger.Printf("snapshot (ID %s) written to %s", s.meta.ID, dstDir)
 	return nil
+}
+
+func (s *Sink) processIncrementalSnapshot(incSnap *IncrementalSnapshot) error {
+	s.logger.Printf("processing incremental snapshot")
+	if err := os.WriteFile(s.walFilePath, incSnap.Data, 0644); err != nil {
+		return fmt.Errorf("error writing WAL data: %v", err)
+	}
+	if err := s.writeMeta(false); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Sink) processFullSnapshot(fullSnap *FullSnapshot) error {
+	s.logger.Printf("processing full snapshot")
+	ngDir, err := s.str.GetNextGenerationDir()
+	if err != nil {
+		return fmt.Errorf("error getting next generation directory: %v", err)
+	}
+	newDir := filepath.Join(ngDir, filepath.Base(s.dir))
+	if err := os.MkdirAll(newDir, 0755); err != nil {
+		return fmt.Errorf("error creating full snapshot directory: %v", err)
+	}
+	if err := os.Rename(s.dir, newDir); err != nil {
+		return fmt.Errorf("error moving full snapshot directory to %s: %v", newDir, err)
+	}
+	s.dir = newDir
+
+	// Write out base SQLite file.
+	dbInfo := fullSnap.GetDb()
+	if dbInfo == nil {
+		return fmt.Errorf("got nil DB info")
+	}
+	sqliteBaseFD, err := os.Create(filepath.Join(s.dir, baseSqliteFile))
+	if err != nil {
+		return fmt.Errorf("error creating SQLite file: %v", err)
+	}
+	if _, err := io.CopyN(sqliteBaseFD, s.dataFD, dbInfo.Size); err != nil {
+		return fmt.Errorf("error writing SQLite file data: %v", err)
+	}
+	sqliteBaseFD.Close()
+
+	// Write out WALs.
+	var walFiles []string
+	for i, wal := range fullSnap.GetWals() {
+		if wal == nil {
+			return fmt.Errorf("got nil WAL")
+		}
+
+		walName := filepath.Join(s.dir, baseSqliteWALFile+fmt.Sprintf("%d", i))
+		walFD, err := os.Create(walName)
+		if err != nil {
+			return fmt.Errorf("error creating WAL file: %v", err)
+		}
+		if _, err := io.CopyN(walFD, s.dataFD, wal.Size); err != nil {
+			return fmt.Errorf("error writing WAL file data: %v", err)
+		}
+		walFD.Close()
+		walFiles = append(walFiles, walName)
+	}
+
+	// Checkpoint the WAL file into the base SQLite file
+	if err := db.ReplayWAL(filepath.Join(s.dir, baseSqliteFile), walFiles, false); err != nil {
+		return fmt.Errorf("error checkpointing WAL: %v", err)
+	}
+
+	if err := s.writeMeta(false); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Sink) writeMeta(full bool) error {
+	fh, err := os.Create(filepath.Join(s.dir, metaFileName))
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	s.meta.Full = full
+
+	// Write out as JSON
+	enc := json.NewEncoder(fh)
+	if err = enc.Encode(s.meta); err != nil {
+		return err
+	}
+
+	if err := fh.Sync(); err != nil {
+		return err
+	}
+	return fh.Close()
+}
+
+func parentDir(dir string) string {
+	return filepath.Dir(dir)
+}
+
+func tmpName(path string) string {
+	return path + tmpSuffix
+}
+
+func nonTmpName(path string) string {
+	return strings.TrimSuffix(path, tmpSuffix)
+}
+
+func moveFromTmp(src string) (string, error) {
+	dst := nonTmpName(src)
+	if err := os.Rename(src, dst); err != nil {
+		return "", err
+	}
+	return dst, nil
+}
+
+func syncDir(dir string) error {
+	fh, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	if err := fh.Sync(); err != nil {
+		return err
+	}
+	return fh.Close()
 }
---
+++ snapshot/store.go
@@ -0,0 +1,285 @@
+package snapshot
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/raft"
+)
+
+const (
+	generationsDir = "generations"
+)
+
+// Meta represents the metadata for a snapshot.
+type Meta struct {
+	raft.SnapshotMeta
+	Full bool
+}
+
+// Store is a store for snapshots.
+type Store struct {
+	rootDir        string
+	generationsDir string
+	logger         *log.Logger
+}
+
+// NewStore creates a new Store object.
+func NewStore(dir string) (*Store, error) {
+	genDir := filepath.Join(dir, generationsDir)
+	if err := os.MkdirAll(genDir, 0755); err != nil {
+		return nil, err
+	}
+	s := &Store{
+		rootDir:        dir,
+		generationsDir: genDir,
+		logger:         log.New(os.Stderr, "[snapshot-store] ", log.LstdFlags),
+	}
+
+	if err := s.check(); err != nil {
+		return nil, fmt.Errorf("check failed: %s", err)
+	}
+	if err := s.ensureFirstGeneration(); err != nil {
+		return nil, fmt.Errorf("creatinmg first generation failed: %s", err)
+	}
+
+	return s, nil
+}
+
+// Create creates a new Sink object, ready for writing a snapshot.
+func (s *Store) Create(version raft.SnapshotVersion, index, term uint64, configuration raft.Configuration,
+	configurationIndex uint64, trans raft.Transport) (raft.SnapshotSink, error) {
+	currGenDir, err := s.GetCurrentGenerationDir()
+	if err != nil {
+		return nil, err
+	}
+	snapshotName := snapshotName(term, index)
+	snapshotPath := filepath.Join(currGenDir, snapshotName+tmpSuffix)
+	if err := os.MkdirAll(snapshotPath, 0755); err != nil {
+		return nil, err
+	}
+
+	meta := &Meta{
+		SnapshotMeta: raft.SnapshotMeta{
+			Index:              index,
+			Term:               term,
+			Configuration:      configuration,
+			ConfigurationIndex: configurationIndex,
+			Version:            version,
+		},
+	}
+
+	return NewSink(s, snapshotPath, meta), nil
+}
+
+// List returns a list of all the snapshots in the Store.
+func (s *Store) List() ([]*raft.SnapshotMeta, error) {
+	gen, err := s.GetCurrentGenerationDir()
+	if err != nil {
+		return nil, err
+	}
+	snapshots, err := s.getSnapshots(gen)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert to the type Raft expects and make only 1 available.
+	var snaps = []*raft.SnapshotMeta{}
+	if len(snapshots) > 0 {
+		snaps = append(snaps, &snapshots[0].SnapshotMeta)
+	}
+	return snaps, nil
+}
+
+// Open opens the snapshot with the given ID.
+func (s *Store) Open(id string) (*raft.SnapshotMeta, io.ReadCloser, error) {
+	return nil, nil, nil
+}
+
+// Dir returns the directory where the snapshots are stored.
+func (s *Store) Dir() string {
+	return s.rootDir
+}
+
+// FullNeeded returns true if the next type of snapshot needed
+// by the Store is a full snapshot.
+func (s *Store) FullNeeded() bool {
+	return false
+}
+
+// GetNextGeneration returns the name of the next generation.
+func (s *Store) GetNextGeneration() (string, error) {
+	generations, err := s.GetGenerations()
+	if err != nil {
+		return "", err
+	}
+	nextGen := 1
+	if len(generations) > 0 {
+		i, err := strconv.Atoi(generations[len(generations)-1])
+		if err != nil {
+			return "", err
+		}
+		nextGen = i + 1
+	}
+	return fmt.Sprintf("%010d", nextGen), nil
+}
+
+// GetNextGenerationDir returns the directory path of the next generation.
+func (s *Store) GetNextGenerationDir() (string, error) {
+	nextGen, err := s.GetNextGeneration()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(s.generationsDir, nextGen), nil
+}
+
+// GetGenerations returns a list of all the generations, sorted
+// from oldest to newest.
+func (s *Store) GetGenerations() ([]string, error) {
+	entries, err := os.ReadDir(s.generationsDir)
+	if err != nil {
+		return nil, err
+	}
+	var generations []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			if _, err := strconv.Atoi(entry.Name()); err != nil {
+				continue
+			}
+			generations = append(generations, entry.Name())
+		}
+	}
+	return generations, nil
+}
+
+func (s *Store) GetCurrentGenerationDir() (string, error) {
+	generations, err := s.GetGenerations()
+	if err != nil {
+		return "", err
+	}
+	if len(generations) == 0 {
+		return "", fmt.Errorf("no generations found")
+	}
+	return filepath.Join(s.generationsDir, generations[len(generations)-1]), nil
+}
+
+func (s *Store) ensureFirstGeneration() error {
+	generations, err := s.GetGenerations()
+	if err != nil {
+		return err
+	}
+	if len(generations) == 0 {
+		if err := os.MkdirAll(filepath.Join(s.generationsDir, "0000000001"), 0755); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// getSnapshots returns a list of all the snapshots in the given directory, sorted from
+// most recently created to oldest created.
+func (s *Store) getSnapshots(dir string) ([]*Meta, error) {
+	snapshots, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	// Populate the metadata
+	var snapMeta []*Meta
+	for _, snap := range snapshots {
+		// Ignore any files
+		if !snap.IsDir() {
+			continue
+		}
+
+		// Ignore any temporary snapshots
+		snapName := snap.Name()
+		if isTmpName(snapName) {
+			continue
+		}
+
+		// Try to read the meta data
+		meta, err := s.readMeta(snapName)
+		if err != nil {
+			return nil, err
+		}
+		snapMeta = append(snapMeta, meta)
+	}
+
+	// Sort the snapshot, reverse so we get new -> old
+	sort.Sort(sort.Reverse(snapMetaSlice(snapMeta)))
+
+	return snapMeta, nil
+}
+
+// readMeta is used to read the meta data for a given named backup
+func (s *Store) readMeta(name string) (*Meta, error) {
+	// Open the meta file
+	metaPath := filepath.Join(s.rootDir, name, metaFileName)
+	fh, err := os.Open(metaPath)
+	if err != nil {
+		return nil, err
+	}
+	defer fh.Close()
+
+	// Read in the JSON
+	meta := &Meta{}
+	dec := json.NewDecoder(fh)
+	if err := dec.Decode(meta); err != nil {
+		return nil, err
+	}
+	return meta, nil
+}
+
+func (s *Store) check() error {
+	return nil
+}
+
+func isTmpName(name string) bool {
+	return filepath.Ext(name) == tmpSuffix
+}
+
+// snapshotName generates a name for the snapshot.
+func snapshotName(term, index uint64) string {
+	now := time.Now()
+	msec := now.UnixNano() / int64(time.Millisecond)
+	return fmt.Sprintf("%d-%d-%d", term, index, msec)
+}
+
+// snapMetaSlice is a sortable slice of walSnapshotMeta, which are sorted
+// by term, index, and then ID. Snapshots are sorted from oldest to newest.
+type snapMetaSlice []*Meta
+
+func (s snapMetaSlice) Len() int {
+	return len(s)
+}
+
+func (s snapMetaSlice) Less(i, j int) bool {
+	if s[i].Term != s[j].Term {
+		return s[i].Term < s[j].Term
+	}
+	if s[i].Index != s[j].Index {
+		return s[i].Index < s[j].Index
+	}
+	return s[i].ID < s[j].ID
+}
+
+func (s snapMetaSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s snapMetaSlice) Contains(id string) bool {
+	for _, snap := range s {
+		if snap.ID == id {
+			return true
+		}
+	}
+	return false
+}
