835b450b9ebfb48cd57eda837876e11b06cea818
Philip O'Toole <philip.otoole@yahoo.com>
2023-08-22 13:19:48 -0400

    Decompose system-level testing

--- system_test/system.go
+++
@@ -1,2 +0,0 @@
-// Package system supports running end-to-end type testing of rqlite
-package system
---
+++ system_test/system/system.go
@@ -0,0 +1,2 @@
+// Package system supports running integration testing of rqlite
+package system
