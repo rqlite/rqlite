26ca4292fa5bb974d1f78a0b387167aaf4f61f1f
Philip O'Toole <philip.otoole@yahoo.com>
2023-10-09 21:36:25 -0400

    Start unit-testing Snapshot Store v2

---
+++ snapshot2/store_test.go
@@ -0,0 +1,41 @@
+package snapshot2
+
+import "testing"
+
+func Test_NewStore(t *testing.T) {
+	dir := t.TempDir()
+	store, err := NewStore(dir)
+	if err != nil {
+		t.Fatalf("Failed to create new store: %v", err)
+	}
+
+	if store.Dir() != dir {
+		t.Errorf("Expected store directory to be %s, got %s", dir, store.Dir())
+	}
+}
+
+func Test_StoreEmpty(t *testing.T) {
+	dir := t.TempDir()
+	store, _ := NewStore(dir)
+
+	snaps, err := store.List()
+	if err != nil {
+		t.Fatalf("Failed to list snapshots: %v", err)
+	}
+	if len(snaps) != 0 {
+		t.Errorf("Expected no snapshots, got %d", len(snaps))
+	}
+
+	_, _, err = store.Open("non-existent")
+	if err == nil {
+		t.Fatalf("Expected error opening non-existent snapshot, got nil")
+	}
+
+	n, err := store.Reap()
+	if err != nil {
+		t.Fatalf("Failed to reap snapshots from empty store: %v", err)
+	}
+	if n != 0 {
+		t.Errorf("Expected no snapshots reaped, got %d", n)
+	}
+}
