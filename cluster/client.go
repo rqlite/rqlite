f7ab671b151dee3eb147ed2cb3bd44977be79d96
Philip O'Toole <philip.otoole@yahoo.com>
2023-11-14 20:29:14 -0500

    Unit test Join on the cluster client

--- cluster/client.go
+++ cluster/client.go
@@ -336,7 +336,7 @@ func (c *Client) Notify(nr *command.NotifyRequest, nodeAddr string, timeout time
 	return nil
 }
 
-// Join joins this node to a cluster
+// Join joins this node to a cluster at the remote address nodeAddr.
 func (c *Client) Join(jr *command.JoinRequest, nodeAddr string, timeout time.Duration) error {
 	conn, err := c.dial(nodeAddr, c.timeout)
 	if err != nil {
