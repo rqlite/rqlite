d36d31d0c4f9b48504bc450ee94687847045e8d9
Philip O'Toole <philip.otoole@yahoo.com>
2023-12-03 20:12:30 -0500

    Check Writer and Compactor

--- db/wal/writer_test.go
+++ db/wal/writer_test.go
@@ -4,13 +4,15 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
+	"io"
 	"os"
 	"testing"
 
 	_ "github.com/rqlite/go-sqlite3"
+	"github.com/rqlite/rqlite/random"
 )
 
-func Test_NewWriter_FullScanner(t *testing.T) {
+func Test_Writer_FullScanner(t *testing.T) {
 	b, err := os.ReadFile("testdata/wal-reader/ok/wal")
 	if err != nil {
 		t.Fatal(err)
@@ -41,7 +43,7 @@ func Test_NewWriter_FullScanner(t *testing.T) {
 	}
 }
 
-func Test_NewWriter_FullScanner_LargeWAL(t *testing.T) {
+func Test_Writer_FullScanner_LargeWAL(t *testing.T) {
 	conn, path := mustCreateWAL(t, 128*1024)
 	defer conn.Close()
 	b, err := os.ReadFile(path)
@@ -75,6 +77,111 @@ func Test_NewWriter_FullScanner_LargeWAL(t *testing.T) {
 	}
 }
 
+// Test_Writer_CompactingScanner tests The CompactingScanner by continuously
+// using it to copy a WAL file, and using that WAL file to apply changes to a
+// second database. The second database should be identical to the first in
+// terms of SQL queries.
+func Test_Writer_CompactingScanner(t *testing.T) {
+	srcDir := t.TempDir()
+	srcDSN := fmt.Sprintf("file:%s", srcDir+"/src.db?_journal_mode=WAL&_synchronous=OFF")
+	srcDB := srcDir + "/src.db"
+	srcWAL := srcDir + "/src.db-wal"
+	srcConn, err := sql.Open("sqlite3", srcDSN)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer srcConn.Close()
+	mustExec(srcConn, "PRAGMA wal_autocheckpoint=0")
+
+	destDir := t.TempDir()
+	destDSN := fmt.Sprintf("file:%s", destDir+"/dest.db")
+	destDB := destDir + "/dest.db"
+	destWAL := destDir + "/dest.db-wal"
+
+	mustExec(srcConn, "CREATE TABLE foo (id INTEGER PRIMARY KEY, name TEXT)")
+
+	// Copy the src database to the dest database to seed the process.
+	mustCopyFile(destDB, srcDB)
+
+	writeRows := func(db *sql.DB, n int) {
+		// Write 1000 random rows to the src database, this data will appear in the WAL.
+		for i := 0; i < 1000; i++ {
+			mustExec(srcConn, fmt.Sprintf(`INSERT INTO foo (name) VALUES ('%s')`, random.String()))
+		}
+		mustExec(srcConn, "PRAGMA wal_checkpoint(FULL)")
+	}
+
+	copyAndCompactWAL := func(sWAL, dWAL string) {
+		// Copy the src WAL to the dest WAL using the CompactingScanner.
+		srcF, err := os.Open(sWAL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer srcF.Close()
+		destF, err := os.Create(dWAL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer destF.Close()
+		s, err := NewCompactingScanner(srcF)
+		if err != nil {
+			t.Fatal(err)
+		}
+		w, err := NewWriter(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = w.WriteTo(destF)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	checkRowCount := func(dsn string, n int) {
+		db, err := sql.Open("sqlite3", destDSN)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer db.Close()
+		rows, err := db.Query("SELECT COUNT(*) FROM foo")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer rows.Close()
+		var count int
+		for rows.Next() {
+			if err := rows.Scan(&count); err != nil {
+				t.Fatal(err)
+			}
+		}
+		if count != n {
+			t.Fatalf("expected %d rows, got %d", n, count)
+		}
+	}
+
+	writeRows(srcConn, 1000)
+	copyAndCompactWAL(srcWAL, destWAL)
+	checkRowCount(srcDSN, 1000)
+	checkRowCount(destDSN, 1000)
+
+	writeRows(srcConn, 1000)
+	copyAndCompactWAL(srcWAL, destWAL)
+	checkRowCount(srcDSN, 2000)
+
+	checkRowCount(destDSN, 2000)
+
+	writeRows(srcConn, 1000)
+	copyAndCompactWAL(srcWAL, destWAL)
+	checkRowCount(srcDSN, 3000)
+	checkRowCount(destDSN, 3000)
+}
+
+func mustExec(db *sql.DB, query string) {
+	if _, err := db.Exec(query); err != nil {
+		panic(err)
+	}
+}
+
 func mustCreateWAL(t *testing.T, size int) (*sql.DB, string) {
 	dir := t.TempDir()
 	rwDSN := fmt.Sprintf("file:%s", dir+"/test.db")
@@ -83,19 +190,13 @@ func mustCreateWAL(t *testing.T, size int) (*sql.DB, string) {
 		panic(err)
 	}
 
-	mustExec := func(query string) {
-		if _, err := rwDB.Exec(query); err != nil {
-			panic(err)
-		}
-	}
-
-	mustExec("PRAGMA journal_mode=WAL")
-	mustExec("PRAGMA wal_autocheckpoint=0")
-	mustExec("PRAGMA synchronous=OFF")
-	mustExec("CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)")
+	mustExec(rwDB, "PRAGMA journal_mode=WAL")
+	mustExec(rwDB, "PRAGMA wal_autocheckpoint=0")
+	mustExec(rwDB, "PRAGMA synchronous=OFF")
+	mustExec(rwDB, "CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)")
 	for {
 		for i := 0; i < 10; i++ {
-			mustExec("INSERT INTO test (name) VALUES ('name')")
+			mustExec(rwDB, "INSERT INTO test (name) VALUES ('name')")
 		}
 		// break if dir+test.db-wal is bigger than size
 		if fi, err := os.Stat(dir + "/test.db-wal"); err != nil {
@@ -107,4 +208,23 @@ func mustCreateWAL(t *testing.T, size int) (*sql.DB, string) {
 		}
 	}
 	return rwDB, dir + "/test.db-wal"
+}
+
+func mustCopyFile(dst, src string) {
+	srcF, err := os.Open(src)
+	if err != nil {
+		panic(err)
+	}
+	defer srcF.Close()
+
+	dstF, err := os.Create(dst)
+	if err != nil {
+		panic(err)
+	}
+	defer dstF.Close()
+
+	_, err = io.Copy(dstF, srcF)
+	if err != nil {
+		panic(err)
+	}
 }
