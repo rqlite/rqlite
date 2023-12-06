f26f15476eb3768d0c6747720c893c904f224c88
Philip O'Toole <philip.otoole@yahoo.com>
2023-12-05 20:07:13 -0500

    Actually add Provider type

---
+++ store/provider.go
@@ -0,0 +1,31 @@
+package store
+
+// Provider implements the uploader Provider interface, allowing the
+// Store to be used as a DataProvider for an uploader.
+type Provider struct {
+	str    *Store
+	vacuum bool
+}
+
+// NewProvider returns a new instance of Provider.
+func NewProvider(s *Store, v bool) *Provider {
+	return &Provider{
+		str:    s,
+		vacuum: v,
+	}
+}
+
+// Provider writes the SQLite database to the given path.
+func (p *Provider) Provide(path string) error {
+	if err := p.str.db.Backup(path); err != nil {
+		return err
+	}
+
+	if p.vacuum {
+		if err := p.str.db.Vacuum(); err != nil {
+			return err
+		}
+	}
+	stats.Add(numProvides, 1)
+	return nil
+}
