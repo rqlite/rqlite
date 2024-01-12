c0d88881bb0bc2a8dc61b329ea26470bc67c9791
Philip O'Toole <philip.otoole@yahoo.com>
2024-01-11 20:04:44 -0500

    Refactor Provider tests

--- store/provider_test.go
+++ store/provider_test.go
@@ -10,191 +10,107 @@ import (
 	command "github.com/rqlite/rqlite/v8/command/proto"
 )
 
+func test_SingleNodeProvide(t *testing.T, vaccuum, compress bool) {
+	s0, ln := mustNewStore(t)
+	defer ln.Close()
+
+	if err := s0.Open(); err != nil {
+		t.Fatalf("failed to open single-node store: %s", err.Error())
+	}
+	if err := s0.Bootstrap(NewServer(s0.ID(), s0.Addr(), true)); err != nil {
+		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
+	}
+	defer s0.Close(true)
+	if _, err := s0.WaitForLeader(10 * time.Second); err != nil {
+		t.Fatalf("Error waiting for leader: %s", err)
+	}
+
+	er := executeRequestFromStrings([]string{
+		`CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`,
+		`INSERT INTO foo(id, name) VALUES(1, "fiona")`,
+	}, false, false)
+	_, err := s0.Execute(er)
+	if err != nil {
+		t.Fatalf("failed to execute on single node: %s", err.Error())
+	}
+	qr := queryRequestFromString("SELECT * FROM foo", false, false)
+	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_NONE
+	r, err := s0.Query(qr)
+	if err != nil {
+		t.Fatalf("failed to query leader node: %s", err.Error())
+	}
+	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+
+	tmpFd := mustCreateTempFD()
+	defer os.Remove(tmpFd.Name())
+	defer tmpFd.Close()
+	provider := NewProvider(s0, vaccuum, compress)
+	if _, err := provider.Provide(tmpFd); err != nil {
+		t.Fatalf("failed to provide SQLite data: %s", err.Error())
+	}
+
+	// Load the provided data into a new store and check it.
+	s1, ln := mustNewStore(t)
+	defer ln.Close()
+
+	if err := s1.Open(); err != nil {
+		t.Fatalf("failed to open single-node store: %s", err.Error())
+	}
+	if err := s1.Bootstrap(NewServer(s1.ID(), s1.Addr(), true)); err != nil {
+		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
+	}
+	defer s1.Close(true)
+	if _, err := s1.WaitForLeader(10 * time.Second); err != nil {
+		t.Fatalf("Error waiting for leader: %s", err)
+	}
+
+	s1file := tmpFd.Name()
+	if compress {
+		unCompressedFile, err := gunzip(tmpFd.Name())
+		if err != nil {
+			t.Fatalf("failed to gunzip provided SQLite data: %s", err.Error())
+		}
+		s1file = unCompressedFile
+	}
+
+	err = s1.Load(loadRequestFromFile(s1file))
+	if err != nil {
+		t.Fatalf("failed to load provided SQLite data: %s", err.Error())
+	}
+	qr = queryRequestFromString("SELECT * FROM foo", false, false)
+	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
+	r, err = s1.Query(qr)
+	if err != nil {
+		t.Fatalf("failed to query leader node: %s", err.Error())
+	}
+	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
+		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
+	}
+}
+
 // Test_SingleNodeProvide tests that the Store correctly implements
 // the Provide method.
 func Test_SingleNodeProvide(t *testing.T) {
-	s0, ln := mustNewStore(t)
-	defer ln.Close()
-
-	if err := s0.Open(); err != nil {
-		t.Fatalf("failed to open single-node store: %s", err.Error())
-	}
-	if err := s0.Bootstrap(NewServer(s0.ID(), s0.Addr(), true)); err != nil {
-		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
-	}
-	defer s0.Close(true)
-	if _, err := s0.WaitForLeader(10 * time.Second); err != nil {
-		t.Fatalf("Error waiting for leader: %s", err)
-	}
-
-	er := executeRequestFromStrings([]string{
-		`CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`,
-		`INSERT INTO foo(id, name) VALUES(1, "fiona")`,
-	}, false, false)
-	_, err := s0.Execute(er)
-	if err != nil {
-		t.Fatalf("failed to execute on single node: %s", err.Error())
-	}
-	qr := queryRequestFromString("SELECT * FROM foo", false, false)
-	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_NONE
-	r, err := s0.Query(qr)
-	if err != nil {
-		t.Fatalf("failed to query leader node: %s", err.Error())
-	}
-	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-
-	tmpFd := mustCreateTempFD()
-	defer os.Remove(tmpFd.Name())
-	defer tmpFd.Close()
-	provider := NewProvider(s0, false, false)
-	if _, err := provider.Provide(tmpFd); err != nil {
-		t.Fatalf("failed to provide SQLite data: %s", err.Error())
-	}
-
-	// Load the provided data into a new store and check it.
-	s1, ln := mustNewStore(t)
-	defer ln.Close()
-
-	if err := s1.Open(); err != nil {
-		t.Fatalf("failed to open single-node store: %s", err.Error())
-	}
-	if err := s1.Bootstrap(NewServer(s1.ID(), s1.Addr(), true)); err != nil {
-		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
-	}
-	defer s1.Close(true)
-	if _, err := s1.WaitForLeader(10 * time.Second); err != nil {
-		t.Fatalf("Error waiting for leader: %s", err)
-	}
-
-	err = s1.Load(loadRequestFromFile(tmpFd.Name()))
-	if err != nil {
-		t.Fatalf("failed to load provided SQLite data: %s", err.Error())
-	}
-	qr = queryRequestFromString("SELECT * FROM foo", false, false)
-	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
-	r, err = s1.Query(qr)
-	if err != nil {
-		t.Fatalf("failed to query leader node: %s", err.Error())
-	}
-	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-}
-
-func Test_SingleNodeProvide_Compress(t *testing.T) {
-	s0, ln := mustNewStore(t)
-	defer ln.Close()
-
-	if err := s0.Open(); err != nil {
-		t.Fatalf("failed to open single-node store: %s", err.Error())
-	}
-	if err := s0.Bootstrap(NewServer(s0.ID(), s0.Addr(), true)); err != nil {
-		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
-	}
-	defer s0.Close(true)
-	if _, err := s0.WaitForLeader(10 * time.Second); err != nil {
-		t.Fatalf("Error waiting for leader: %s", err)
-	}
-
-	er := executeRequestFromStrings([]string{
-		`CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`,
-		`INSERT INTO foo(id, name) VALUES(1, "fiona")`,
-	}, false, false)
-	_, err := s0.Execute(er)
-	if err != nil {
-		t.Fatalf("failed to execute on single node: %s", err.Error())
-	}
-	qr := queryRequestFromString("SELECT * FROM foo", false, false)
-	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_NONE
-	r, err := s0.Query(qr)
-	if err != nil {
-		t.Fatalf("failed to query leader node: %s", err.Error())
-	}
-	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-
-	tmpFd := mustCreateTempFD()
-	defer os.Remove(tmpFd.Name())
-	defer tmpFd.Close()
-	provider := NewProvider(s0, false, true)
-	if _, err := provider.Provide(tmpFd); err != nil {
-		t.Fatalf("failed to provide SQLite data: %s", err.Error())
-	}
-
-	unCompressedFile, err := gunzip(tmpFd.Name())
-	if err != nil {
-		t.Fatalf("failed to gunzip provided SQLite data: %s", err.Error())
-	}
-
-	// Load the provided data into a new store and check it.
-	s1, ln := mustNewStore(t)
-	defer ln.Close()
-
-	if err := s1.Open(); err != nil {
-		t.Fatalf("failed to open single-node store: %s", err.Error())
-	}
-	if err := s1.Bootstrap(NewServer(s1.ID(), s1.Addr(), true)); err != nil {
-		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
-	}
-	defer s1.Close(true)
-	if _, err := s1.WaitForLeader(10 * time.Second); err != nil {
-		t.Fatalf("Error waiting for leader: %s", err)
-	}
-
-	err = s1.Load(loadRequestFromFile(unCompressedFile))
-	if err != nil {
-		t.Fatalf("failed to load provided SQLite data: %s", err.Error())
-	}
-	qr = queryRequestFromString("SELECT * FROM foo", false, false)
-	qr.Level = command.QueryRequest_QUERY_REQUEST_LEVEL_STRONG
-	r, err = s1.Query(qr)
-	if err != nil {
-		t.Fatalf("failed to query leader node: %s", err.Error())
-	}
-	if exp, got := `["id","name"]`, asJSON(r[0].Columns); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-	if exp, got := `[[1,"fiona"]]`, asJSON(r[0].Values); exp != got {
-		t.Fatalf("unexpected results for query\nexp: %s\ngot: %s", exp, got)
-	}
-}
-
-// Test_SingleNodeProvideNoData checks the Provide method operates
-// correctly when there is no data to provide.
-func Test_SingleNodeProvideNoData(t *testing.T) {
-	s, ln := mustNewStore(t)
-	defer ln.Close()
-
-	if err := s.Open(); err != nil {
-		t.Fatalf("failed to open single-node store: %s", err.Error())
-	}
-	if err := s.Bootstrap(NewServer(s.ID(), s.Addr(), true)); err != nil {
-		t.Fatalf("failed to bootstrap single-node store: %s", err.Error())
-	}
-	defer s.Close(true)
-	if _, err := s.WaitForLeader(10 * time.Second); err != nil {
-		t.Fatalf("Error waiting for leader: %s", err)
-	}
-
-	tmpFd := mustCreateTempFD()
-	defer os.Remove(tmpFd.Name())
-	defer tmpFd.Close()
-	provider := NewProvider(s, false, false)
-	if _, err := provider.Provide(tmpFd); err != nil {
-		t.Fatalf("store failed to provide: %s", err.Error())
-	}
+	t.Run("NoVacuumNoCompress", func(t *testing.T) {
+		test_SingleNodeProvide(t, false, false)
+	})
+	t.Run("VacuumNoCompress", func(t *testing.T) {
+		test_SingleNodeProvide(t, true, false)
+	})
+	t.Run("NoVacuumCompress", func(t *testing.T) {
+		test_SingleNodeProvide(t, false, true)
+	})
+	t.Run("VacuumCompress", func(t *testing.T) {
+		test_SingleNodeProvide(t, true, true)
+	})
 }
 
 func Test_SingleNodeProvideLastModified(t *testing.T) {
