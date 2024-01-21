7feabe6c8bf9fe162e2c01cfe8800c69bc9cfd24
Philip O'Toole <philip.otoole@yahoo.com>
2024-01-20 20:01:05 -0500

    GoDoc

--- db/swappable_db.go
+++ db/swappable_db.go
@@ -9,11 +9,14 @@ import (
 	command "github.com/rqlite/rqlite/v8/command/proto"
 )
 
+// SwappableDB is a wrapper around DB that allows the underlying database to be swapped out
+// in a thread-safe manner.
 type SwappableDB struct {
 	db   *DB
 	dbMu sync.RWMutex
 }
 
+// OpenSwappable returns a new SwappableDB instance, which opens the database at the given path.
 func OpenSwappable(dbPath string, fkEnabled, wal bool) (*SwappableDB, error) {
 	db, err := Open(dbPath, fkEnabled, wal)
 	if err != nil {
@@ -22,6 +25,7 @@ func OpenSwappable(dbPath string, fkEnabled, wal bool) (*SwappableDB, error) {
 	return &SwappableDB{db: db}, nil
 }
 
+// Swap swaps the underlying database with that at the given path.
 func (s *SwappableDB) Swap(path string, fkConstraints, walEnabled bool) error {
 	if !IsValidSQLiteFile(path) {
 		return fmt.Errorf("invalid SQLite data")
@@ -48,84 +52,98 @@ func (s *SwappableDB) Swap(path string, fkConstraints, walEnabled bool) error {
 	return nil
 }
 
+// Close closes the underlying database.
 func (s *SwappableDB) Close() error {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Close()
 }
 
+// Stats returns the underlying database's stats.
 func (s *SwappableDB) Stats() (map[string]interface{}, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Stats()
 }
 
+// Request calls Request on the underlying database.
 func (s *SwappableDB) Request(req *command.Request, xTime bool) ([]*command.ExecuteQueryResponse, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Request(req, xTime)
 }
 
+// Execute calls Execute on the underlying database.
 func (s *SwappableDB) Execute(ex *command.Request, xTime bool) ([]*command.ExecuteResult, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Execute(ex, xTime)
 }
 
+// Query calls Query on the underlying database.
 func (s *SwappableDB) Query(q *command.Request, xTime bool) ([]*command.QueryRows, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Query(q, xTime)
 }
 
+// VacuumInto calls VacuumInto on the underlying database.
 func (s *SwappableDB) VacuumInto(path string) error {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.VacuumInto(path)
 }
 
+// Backup calls Backup on the underlying database.
 func (s *SwappableDB) Backup(path string, vacuum bool) error {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Backup(path, vacuum)
 }
 
+// Serialize calls Serialize on the underlying database.
 func (s *SwappableDB) Serialize() ([]byte, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Serialize()
 }
 
+// StmtReadOnly calls StmtReadOnly on the underlying database.
 func (s *SwappableDB) StmtReadOnly(sql string) (bool, error) {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.StmtReadOnly(sql)
 }
 
+// Checkpoint calls Checkpoint on the underlying database.
 func (s *SwappableDB) Checkpoint() error {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Checkpoint()
 }
 
+// Path calls Path on the underlying database.
 func (s *SwappableDB) Path() string {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Path()
 }
 
+// Dump calls Dump on the underlying database.
 func (s *SwappableDB) Dump(w io.Writer) error {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.Dump(w)
 }
 
+// FKEnabled calls FKEnabled on the underlying database.
 func (s *SwappableDB) FKEnabled() bool {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
 	return s.db.fkEnabled
 }
 
+// WALEnabled calls WALEnabled on the underlying database.
 func (s *SwappableDB) WALEnabled() bool {
 	s.dbMu.RLock()
 	defer s.dbMu.RUnlock()
