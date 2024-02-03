21313a56bf18a12144ba5f77003c07268f23d74f
Philip O'Toole <philip.otoole@yahoo.com>
2024-02-03 09:20:10 -0500

    Shorten time

--- system_test/single_node_test.go
+++ system_test/single_node_test.go
@@ -573,7 +573,7 @@ func Test_SingleNodeQueryTimeout(t *testing.T) {
 	q := `SELECT key1, key_id, key2, key3, key4, key5, key6, data
 	FROM test_table
 	ORDER BY key2 ASC`
-	_, err = node.QueryWithTimeout(q, 1*time.Millisecond)
+	_, err = node.QueryWithTimeout(q, 1*time.Microsecond)
 	if err == nil {
 		t.Fatalf("query succeeded when it should have timed out")
 	}
