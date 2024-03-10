17b10c20de585e40f415e598c299a95c311aec44
Philip O'Toole <philip.otoole@yahoo.com>
2024-03-10 14:05:55 -0400

    More test fixes

--- system_test/single_node_test.go
+++ system_test/single_node_test.go
@@ -677,7 +677,7 @@ func Test_SingleNode_RETURNING(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to INSERT record: %s", err.Error())
 	}
-	if got, exp := res, `{"results":[{"columns":["id","name"],"types":["integer","text"],"values":[[1,"fiona"]]}]}`; got != exp {
+	if got, exp := res, `{"results":[{"columns":["id","name"],"types":["integer","text"],"values":[[4,"alice"]]},{"last_insert_id":5,"rows_affected":1}]}`; got != exp {
 		t.Fatalf("wrong execute-multi results for RETURNING, exp %s, got %s", exp, got)
 	}
 }
