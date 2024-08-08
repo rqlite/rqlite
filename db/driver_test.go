d0411200bdb69b967ed068a3211e1ba4247d98a4
Philip O'Toole <philip.otoole@yahoo.com>
2024-08-08 15:23:13 -0400

    Unit test DB Driver

---
+++ db/driver_test.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"testing"
+)
+
+func Test_DefaultDriver(t *testing.T) {
+	d := DefaultDriver()
+	if d == nil {
+		t.Fatalf("DefaultDriver returned nil")
+	}
+	if d.Name() != defaultDriverName {
+		t.Fatalf("DefaultDriver returned incorrect name: %s", d.Name())
+	}
+
+	// Call it again, make sure it doesn't panic.
+	d = DefaultDriver()
+	if d == nil {
+		t.Fatalf("DefaultDriver returned nil")
+	}
+}
+
+func Test_NewDriver(t *testing.T) {
+	name := "test-driver"
+	extensions := []string{"test1", "test2"}
+	d := NewDriver(name, extensions)
+	if d == nil {
+		t.Fatalf("NewDriver returned nil")
+	}
+	if d.Name() != name {
+		t.Fatalf("NewDriver returned incorrect name: %s", d.Name())
+	}
+	if len(d.Extensions()) != 2 {
+		t.Fatalf("NewDriver returned incorrect extensions: %v", d.Extensions())
+	}
+}
