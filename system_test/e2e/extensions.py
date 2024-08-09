eb80e8194a59f0a7fcbbb6b2a427ab630748794f
Philip O'Toole <philip.otoole@yahoo.com>
2024-08-09 14:05:27 -0400

    End-to-end test zipped extensions

--- system_test/e2e/extensions.py
+++ system_test/e2e/extensions.py
@@ -13,10 +13,27 @@ from helpers import Node, Cluster, d_
 
 RQLITED_PATH = os.environ['RQLITED_PATH']
 EXTENSIONS_PATH = os.environ['EXTENSIONS_PATH']
+EXTENSIONS_PATH_ZIP = os.environ['EXTENSIONS_PATH_ZIP']
 
 class TestExtensions(unittest.TestCase):
   def setUp(self):
     n0 = Node(RQLITED_PATH, '0', extensions_path=EXTENSIONS_PATH)
+    n0.start()
+    n0.wait_for_leader()
+    self.cluster = Cluster([n0])
+
+  def tearDown(self):
+    self.cluster.deprovision()
+
+  def test_rot13(self):
+    n = self.cluster.wait_for_leader()
+    j = n.query('SELECT rot13("hello")')
+    expected = d_('{"results": [{"columns": ["rot13(\\"hello\\")"], "types": ["text"], "values": [["uryyb"]]}]}')
+    self.assertEqual(j, expected)
+
+class TestExtensions_Zipped(unittest.TestCase):
+  def setUp(self):
+    n0 = Node(RQLITED_PATH, '0', extensions_path=EXTENSIONS_PATH_ZIP)
     n0.start()
     n0.wait_for_leader()
     self.cluster = Cluster([n0])
