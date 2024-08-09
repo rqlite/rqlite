a9d39577332dfdf7756b803eae4efafad61b5281
Philip O'Toole <philip.otoole@yahoo.com>
2024-08-09 11:20:17 -0400

    More generic flag for Extensions location

--- cmd/rqlited/flags.go
+++ cmd/rqlited/flags.go
@@ -40,8 +40,8 @@ type Config struct {
 	// DataPath is path to node data. Always set.
 	DataPath string
 
-	// ExtensionsDir is the path to the directory containing SQLite extensions.
-	ExtensionsDir string `dirpath:"true"`
+	// ExtensionsPath is the path to the directory containing SQLite extensions.
+	ExtensionsPath string `dirpath:"true"`
 
 	// HTTPAddr is the bind network address for the HTTP Server.
 	// It never includes a trailing HTTP or HTTPS.
@@ -477,7 +477,7 @@ func ParseFlags(name, desc string, build *BuildInfo) (*Config, error) {
 	fs := flag.NewFlagSet(name, flag.ExitOnError)
 
 	fs.StringVar(&config.NodeID, "node-id", "", "Unique ID for node. If not set, set to advertised Raft address")
-	fs.StringVar(&config.ExtensionsDir, "extensions-dir", "", "Path to directory containing SQLite extensions which are to be loaded")
+	fs.StringVar(&config.ExtensionsPath, "extensions-path", "", "Path to directory containing SQLite extensions which are to be loaded")
 	fs.StringVar(&config.HTTPAddr, HTTPAddrFlag, "localhost:4001", "HTTP server bind address. To enable HTTPS, set X.509 certificate and key")
 	fs.StringVar(&config.HTTPAdv, HTTPAdvAddrFlag, "", "Advertised HTTP address. If not set, same as HTTP server bind address")
 	fs.StringVar(&config.HTTPAllowOrigin, "http-allow-origin", "", "Value to set for Access-Control-Allow-Origin HTTP header")
