75c5a256a1a1e0836fbfb9bd59b0602b17304573
Philip O'Toole <philip.otoole@yahoo.com>
2024-08-22 21:40:46 -0400

    Tighten up docker entry script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,49 +11,39 @@ contains() {
 	return 0
 }
 
-DEFAULT_NODE_ID=`hostname`
-DEFAULT_ADV_ADDRESS=`hostname -f`
+DEFAULT_NODE_ID=$(hostname)
+DEFAULT_ADV_ADDRESS=$(hostname -f)
 
 CMD=$1
 ARGS="$@"
 
 contains "-http-addr" $ARGS
 if [ $? -eq 0 ]; then
-	if [ -z "$HTTP_ADDR" ]; then
-		HTTP_ADDR="0.0.0.0:4001"
-	fi
+	HTTP_ADDR="${HTTP_ADDR:-0.0.0.0:4001}"
 	http_addr="-http-addr $HTTP_ADDR"
 fi
 
 contains "-http-adv-addr" $ARGS
 if [ $? -eq 0 ]; then
-	if [ -z "$HTTP_ADV_ADDR" ]; then
-		HTTP_ADV_ADDR=$DEFAULT_ADV_ADDRESS:4001
-	fi
+	HTTP_ADV_ADDR="${HTTP_ADV_ADDR:-$DEFAULT_ADV_ADDRESS:4001}"
 	http_adv_addr="-http-adv-addr $HTTP_ADV_ADDR"
 fi
 
-contains "-raft-adv" $ARGS
+contains "-raft-addr" $ARGS
 if [ $? -eq 0 ]; then
-	if [ -z "$RAFT_ADDR" ]; then
-		RAFT_ADDR="0.0.0.0:4002"
-	fi
+	RAFT_ADDR="${RAFT_ADDR:-0.0.0.0:4002}"
 	raft_addr="-raft-addr $RAFT_ADDR"
 fi
 
 contains "-raft-adv-addr" $ARGS
 if [ $? -eq 0 ]; then
-	if [ -z "$RAFT_ADV_ADDR" ]; then
-		RAFT_ADV_ADDR=$DEFAULT_ADV_ADDRESS:4002
-	fi
+	RAFT_ADV_ADDR="${RAFT_ADV_ADDR:-$DEFAULT_ADV_ADDRESS:4002}"
 	raft_adv_addr="-raft-adv-addr $RAFT_ADV_ADDR"
 fi
 
 contains "-node-id" $ARGS
 if [ $? -eq 0 ]; then
-	if [ -z "$NODE_ID" ]; then
-		NODE_ID="$DEFAULT_NODE_ID"
-	fi
+	NODE_ID="${NODE_ID:-$DEFAULT_NODE_ID}"
 	node_id="-node-id $NODE_ID"
 fi
 
@@ -61,7 +51,7 @@ extensions_path=""
 if [ -n "$SQLITE_EXTENSIONS" ]; then
 	contains "-extensions-path" $ARGS
 	if [ $? -eq 1 ]; then
-		echo "Setting both -extensions-path and SQLITE_EXTENSIONS is not allowed"
+		printf "Setting both -extensions-path and SQLITE_EXTENSIONS is not allowed\n"
   		exit 1
 	fi
 
@@ -87,7 +77,7 @@ fi
 if [ -n "$CUSTOM_SQLITE_EXTENSIONS_PATH" ]; then
 	contains "-extensions-path" $ARGS
 	if [ $? -eq 1 ]; then
-		echo "Setting both -extensions-path and CUSTOM_SQLITE_EXTENSIONS_PATH is not allowed"
+		printf "Setting both -extensions-path and CUSTOM_SQLITE_EXTENSIONS_PATH is not allowed\n"
   		exit 1
 	fi
 
@@ -108,28 +98,24 @@ fi
 #
 # This is kind of a hack. If anyone knows a better way file
 # a GitHub issue at https://github.com/rqlite/rqlite.
-if [ -n "$KUBERNETES_SERVICE_HOST" ]; then
-      if [ -z "$START_DELAY" ]; then
-            START_DELAY=5
-      fi
+if [ -n "$KUBERNETES_SERVICE_HOST" ] && [ -z "$START_DELAY" ]; then
+    START_DELAY=5
 fi
-if [ -n "$START_DELAY" ]; then
-      sleep "$START_DELAY"
-fi
+[ -n "$START_DELAY" ] && sleep "$START_DELAY"
 
 RQLITED=/bin/rqlited
 rqlited_commands="$RQLITED $node_id $http_addr $http_adv_addr $raft_addr $raft_adv_addr $extensions_path_flag"
 
-if [ -z "$DATA_DIR" ]; then
-	DATA_DIR="/rqlite/file/data"
-fi
-data_dir="$DATA_DIR"
+data_dir="${DATA_DIR:-/rqlite/file/data}"
 
-if [ "$CMD" = "run" ]; then
-        set -- $rqlited_commands $data_dir
-elif [ "$(expr substr "$CMD" 1 1)" = "-" ]; then
-        # User is passing some options, so merge them.
-        set -- $rqlited_commands $@ $data_dir
-fi
+case "$CMD" in
+    run)
+        set -- $rqlited_commands "$data_dir"
+        ;;
+    -*)
+		 # User is passing some options, so merge them.
+        set -- $rqlited_commands "$@" "$data_dir"
+        ;;
+esac
 
 exec "$@"
