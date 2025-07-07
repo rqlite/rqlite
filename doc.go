d363890a1d6f2452fd92553d4857e13195f5e95d
Philip O'Toole <philip.otoole@yahoo.com>
2025-07-07 11:21:52 -0400

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 
-// Copyright (c) 2014-2023 Philip O'Toole
+// Copyright (c) 2014-2025 Philip O'Toole
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
