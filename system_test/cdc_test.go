2aeeaef5dbdfb16522a28cef7571e1c5daed63ea
Philip O'Toole <philip.otoole@yahoo.com>
2025-08-24 13:31:22 -0400

    CDC Service exposes its channel

--- system_test/cdc_test.go
+++ system_test/cdc_test.go
@@ -2,32 +2,13 @@ package system
 
 import (
 	"testing"
-	"time"
-
-	"github.com/rqlite/rqlite/v8/cdc"
-	"github.com/rqlite/rqlite/v8/command/proto"
 )
 
 func Test_CDC_SingleNode(t *testing.T) {
 	node := mustNewLeaderNode("node1")
 	defer node.Deprovision()
 
-	ch := make(chan *proto.CDCIndexedEventGroup, 5)
-	cdcCfg := cdc.DefaultConfig()
-	cdcCfg.LogOnly = true
-	cdcCluster := cdc.NewCDCCluster(node.Store, node.Cluster, node.Client)
-	cdcService, err := cdc.NewService(node.ID, node.Dir, cdcCluster, ch, cdcCfg)
-	if err != nil {
-		t.Fatalf("failed to create CDC service: %v", err)
-	}
-	if err := cdcService.Start(); err != nil {
-		t.Fatalf("failed to start CDC service: %v", err)
-	}
-	if err := node.Store.EnableCDC(ch, false); err != nil {
-		t.Fatalf("failed to enable CDC: %v", err)
-	}
-
-	_, err = node.Execute(`CREATE TABLE foo (id integer not null primary key, name text)`)
+	_, err := node.Execute(`CREATE TABLE foo (id integer not null primary key, name text)`)
 	if err != nil {
 		t.Fatalf("failed to create table: %v", err)
 	}
@@ -43,6 +24,4 @@ func Test_CDC_SingleNode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to delete data: %v", err)
 	}
-
-	time.Sleep(5 * time.Second) // Allow time for CDC to process events
 }
