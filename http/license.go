44f25e3c2c9de3dd1e81366280da409a3c65eee8
Philip O'Toole <philip.otoole@yahoo.com>
2025-11-07 09:47:56 -0500

    Add licenses

--- http/license.go
+++
@@ -1,7 +0,0 @@
-package http
-
-// LicenseText contains the license information for rqlite and its dependencies.
-const LicenseText = `This is a dummy license file.
-
-This will be replaced with actual license information in the future.
-`
