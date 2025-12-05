75886cdf4df982ebd20bc234c4c9c55fb8660b84
Philip O'Toole <philip.otoole@yahoo.com>
2025-12-05 13:21:41 -0500

    Simplify type checks in comparators.go

--- command/comparators.go
+++ command/comparators.go
@@ -22,34 +22,19 @@ func CDCValueEqual(v1, v2 *proto.CDCValue) bool {
 	switch v1.GetValue().(type) {
 	case *proto.CDCValue_I:
 		v2i, ok := v2.GetValue().(*proto.CDCValue_I)
-		if !ok {
-			return false
-		}
-		return v1.GetI() == v2i.I
+		return ok && v1.GetI() == v2i.I
 	case *proto.CDCValue_D:
 		v2d, ok := v2.GetValue().(*proto.CDCValue_D)
-		if !ok {
-			return false
-		}
-		return v1.GetD() == v2d.D
+		return ok && v1.GetD() == v2d.D
 	case *proto.CDCValue_S:
 		v2s, ok := v2.GetValue().(*proto.CDCValue_S)
-		if !ok {
-			return false
-		}
-		return v1.GetS() == v2s.S
+		return ok && v1.GetS() == v2s.S
 	case *proto.CDCValue_B:
 		v2b, ok := v2.GetValue().(*proto.CDCValue_B)
-		if !ok {
-			return false
-		}
-		return v1.GetB() == v2b.B
+		return ok && v1.GetB() == v2b.B
 	case *proto.CDCValue_Y:
 		v2y, ok := v2.GetValue().(*proto.CDCValue_Y)
-		if !ok {
-			return false
-		}
-		return bytes.Equal(v1.GetY(), v2y.Y)
+		return ok && bytes.Equal(v1.GetY(), v2y.Y)
 	default:
 		return false
 	}
