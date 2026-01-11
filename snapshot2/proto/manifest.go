e21fcc662e27634e83ea7ff022f178af827411ad
Philip O'Toole <philip.otoole@yahoo.com>
2026-01-11 15:30:35 -0500

    GoDoc

--- snapshot2/proto/manifest.go
+++ snapshot2/proto/manifest.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rqlite/rqlite/v9/internal/rsum"
 )
 
+// NewSnapshotDBFileFromFile creates a SnapshotDBFile manifest entry from the given file path.
 func NewSnapshotDBFileFromFile(path string, crc32 bool) (*SnapshotDBFile, error) {
 	s := &SnapshotDBFile{}
 	info, err := os.Stat(path)
@@ -24,6 +25,7 @@ func NewSnapshotDBFileFromFile(path string, crc32 bool) (*SnapshotDBFile, error)
 	return s, nil
 }
 
+// NewSnapshotWALFileFromFile creates a SnapshotWALFile manifest entry from the given file path.
 func NewSnapshotWALFileFromFile(path string, crc32 bool) (*SnapshotWALFile, error) {
 	s := &SnapshotWALFile{}
 	info, err := os.Stat(path)
