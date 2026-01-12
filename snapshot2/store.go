f1deb2c3f5e133da26c3045a87993e8ec793fe15
Philip O'Toole <philip.otoole@yahoo.com>
2026-01-11 21:47:54 -0500

    Snapshot meta

--- snapshot2/store.go
+++ snapshot2/store.go
@@ -11,12 +11,29 @@ import (
 	"time"
 
 	"github.com/hashicorp/raft"
+	"github.com/rqlite/rqlite/v9/db"
 )
 
 const (
 	metaFileName = "meta.json"
 	tmpSuffix    = ".tmp"
 )
+
+type SnapshotMetaType int
+
+// SnapshotMetaType is an enum
+const (
+	SnapshotMetaTypeUnknown SnapshotMetaType = iota
+	SnapshotMetaTypeFull
+	SnapshotMetaTypeIncremental
+)
+
+// SnapshotMeta represents metadata about a snapshot.
+type SnapshotMeta struct {
+	*raft.SnapshotMeta
+	Filename string
+	Type     SnapshotMetaType
+}
 
 // Store stores snapshots in the Raft system.
 type Store struct {
@@ -68,7 +85,11 @@ func (s *Store) Create(version raft.SnapshotVersion, index, term uint64, configu
 
 // List returns the list of available snapshots.
 func (s *Store) List() ([]*raft.SnapshotMeta, error) {
-	return getSnapshots(s.dir)
+	ms, err := getSnapshots(s.dir)
+	if err != nil {
+		return nil, err
+	}
+	return snapMetaSlice(ms).RaftMetaSlice(), nil
 }
 
 // Open opens the snapshot with the given ID for reading.
@@ -85,7 +106,7 @@ func (s *Store) check() error {
 
 // getSnapshots returns the list of snapshots in the given directory,
 // sorted from newest to oldest.
-func getSnapshots(dir string) ([]*raft.SnapshotMeta, error) {
+func getSnapshots(dir string) ([]*SnapshotMeta, error) {
 	// Get the eligible snapshots
 	snapshots, err := os.ReadDir(dir)
 	if err != nil {
@@ -93,7 +114,7 @@ func getSnapshots(dir string) ([]*raft.SnapshotMeta, error) {
 	}
 
 	// Populate the metadata
-	var snapMeta []*raft.SnapshotMeta
+	var snapMeta []*SnapshotMeta
 	for _, snap := range snapshots {
 		// Snapshots are stored in directories, ignore any files.
 		if !snap.IsDir() {
@@ -101,17 +122,24 @@ func getSnapshots(dir string) ([]*raft.SnapshotMeta, error) {
 		}
 
 		// Ignore any temporary snapshots
-		dirName := snap.Name()
-		if isTmpName(dirName) {
+		if isTmpName(snap.Name()) {
 			continue
 		}
 
-		// Try to read the meta data
-		meta, err := readMeta(filepath.Join(dir, dirName))
+		// Read the meta data
+		meta, err := readMeta(filepath.Join(dir, snap.Name()))
 		if err != nil {
-			return nil, fmt.Errorf("failed to read meta for snapshot %s: %s", dirName, err)
+			return nil, fmt.Errorf("failed to read meta for snapshot %s: %s", snap.Name(), err)
 		}
 
+		path := filepath.Join(dir, snap.Name(), meta.Filename)
+		if db.IsValidSQLiteFile(path) {
+			meta.Type = SnapshotMetaTypeFull
+		} else if db.IsValidSQLiteWALFile(path) {
+			meta.Type = SnapshotMetaTypeIncremental
+		} else {
+			return nil, fmt.Errorf("snapshot %s does not contain a valid SQLite or WAL file", snap.Name())
+		}
 		snapMeta = append(snapMeta, meta)
 	}
 
@@ -119,7 +147,7 @@ func getSnapshots(dir string) ([]*raft.SnapshotMeta, error) {
 	return snapMeta, nil
 }
 
-type cmpSnapshotMeta raft.SnapshotMeta
+type cmpSnapshotMeta SnapshotMeta
 
 func (c *cmpSnapshotMeta) Less(other *cmpSnapshotMeta) bool {
 	if c.Term != other.Term {
@@ -131,7 +159,7 @@ func (c *cmpSnapshotMeta) Less(other *cmpSnapshotMeta) bool {
 	return c.ID < other.ID
 }
 
-type snapMetaSlice []*raft.SnapshotMeta
+type snapMetaSlice []*SnapshotMeta
 
 // Len implements the sort interface for snapMetaSlice.
 func (s snapMetaSlice) Len() int {
@@ -150,6 +178,18 @@ func (s snapMetaSlice) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
+// RaftMetaSlice converts the snapMetaSlice to a slice of raft.SnapshotMeta.
+func (s snapMetaSlice) RaftMetaSlice() []*raft.SnapshotMeta {
+	if s == nil {
+		return nil
+	}
+	r := make([]*raft.SnapshotMeta, len(s))
+	for i, sm := range s {
+		r[i] = sm.SnapshotMeta
+	}
+	return r
+}
+
 // snapshotName generates a name for the snapshot.
 func snapshotName(term, index uint64) string {
 	now := time.Now()
@@ -163,14 +203,14 @@ func metaPath(dir string) string {
 }
 
 // readMeta is used to read the meta data in a given snapshot directory.
-func readMeta(dir string) (*raft.SnapshotMeta, error) {
+func readMeta(dir string) (*SnapshotMeta, error) {
 	fh, err := os.Open(metaPath(dir))
 	if err != nil {
 		return nil, err
 	}
 	defer fh.Close()
 
-	meta := &raft.SnapshotMeta{}
+	meta := &SnapshotMeta{}
 	dec := json.NewDecoder(fh)
 	if err := dec.Decode(meta); err != nil {
 		return nil, err
