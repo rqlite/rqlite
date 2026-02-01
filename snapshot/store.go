1a7d6e12788ddf3374859527b3891d88e3be4470
Philip O'Toole <philip.otoole@yahoo.com>
2026-02-01 11:06:22 -0500

    Move Snapshot utils to own source file

--- snapshot/store.go
+++ snapshot/store.go
@@ -8,8 +8,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"runtime"
-	"strings"
 	"sync"
 	"time"
 
@@ -455,87 +453,6 @@ func snapshotName(term, index uint64) string {
 	now := time.Now()
 	msec := now.UnixNano() / int64(time.Millisecond)
 	return fmt.Sprintf("%d-%d-%d", term, index, msec)
-}
-
-func parentDir(dir string) string {
-	return filepath.Dir(dir)
-}
-
-func tmpName(path string) string {
-	return path + tmpSuffix
-}
-
-func nonTmpName(path string) string {
-	return strings.TrimSuffix(path, tmpSuffix)
-}
-
-func isTmpName(name string) bool {
-	return filepath.Ext(name) == tmpSuffix
-}
-
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	return !os.IsNotExist(err)
-}
-
-func dirExists(path string) bool {
-	stat, err := os.Stat(path)
-	return err == nil && stat.IsDir()
-}
-
-func dirIsEmpty(dir string) (bool, error) {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return false, err
-	}
-	return len(files) == 0, nil
-}
-
-func syncDir(dir string) error {
-	fh, err := os.Open(dir)
-	if err != nil {
-		return err
-	}
-	defer fh.Close()
-	return fh.Sync()
-}
-
-func removeDirSync(dir string) error {
-	if err := os.RemoveAll(dir); err != nil {
-		return err
-	}
-	return syncDirParentMaybe(dir)
-}
-
-// syncDirParentMaybe syncs the parent directory of the given
-// directory, but only on non-Windows platforms.
-func syncDirParentMaybe(dir string) error {
-	if runtime.GOOS == "windows" {
-		return nil
-	}
-	return syncDir(parentDir(dir))
-}
-
-// syncDirMaybe syncs the given directory, but only on non-Windows platforms.
-func syncDirMaybe(dir string) error {
-	if runtime.GOOS == "windows" {
-		return nil
-	}
-	return syncDir(dir)
-}
-
-// removeAllPrefix removes all files in the given directory that have the given prefix.
-func removeAllPrefix(path, prefix string) error {
-	files, err := filepath.Glob(filepath.Join(path, prefix) + "*")
-	if err != nil {
-		return err
-	}
-	for _, f := range files {
-		if err := os.RemoveAll(f); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // writeMeta is used to write the meta data in a given snapshot directory.
