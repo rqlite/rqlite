bb1d547653c8c83c6634a7caa1be529f457490c6
Philip O'Toole <philip.otoole@yahoo.com>
2026-02-16 14:44:29 -0500

    End-to-end upgrade from v9

--- system_test/e2e/upgrade.py
+++ system_test/e2e/upgrade.py
@@ -72,5 +72,57 @@ class TestUpgrade_v8_LoadChunk(unittest.TestCase):
   def tearDown(self):
     deprovision_node(self.n)
 
+class TestUpgrade_v9(unittest.TestCase):
+  '''Test that a v8 cluster can be upgraded to this version code'''
+  def poll_query(self, node, query, exp):
+    t = 0
+    while True:
+      if t > TIMEOUT:
+        raise Exception('timeout waiting for node %s to return correct results' % node.node_id)
+      j = node.query(query, level='none')
+      if j == exp:
+        break
+      time.sleep(1)
+      t+=1
+
+  def test(self):
+    dir1 = copy_dir_to_temp('testdata/v9/data.1')
+    n0 = Node(RQLITED_PATH, '1', api_addr='localhost:4001', raft_addr='localhost:4002', dir=dir1,
+              raft_snap_int='100ms', raft_snap_threshold='5')
+    n0.start()
+
+    dir2 = copy_dir_to_temp('testdata/v9/data.2')
+    n1 = Node(RQLITED_PATH, '2', api_addr='localhost:4003', raft_addr='localhost:4004', dir=dir2,
+              raft_snap_int='100ms', raft_snap_threshold='5')
+    n1.start()
+
+    dir3 = copy_dir_to_temp('testdata/v9/data.3')
+    n2 = Node(RQLITED_PATH, '3', api_addr='localhost:4005', raft_addr='localhost:4006', dir=dir3,
+              raft_snap_int='100ms', raft_snap_threshold='5')
+    n2.start()
+
+    self.cluster = Cluster([n0, n1, n2])
+    l = self.cluster.wait_for_leader()
+
+    # Check that each node performed the upgrade.
+    for n in self.cluster.nodes:
+      self.assertEqual(n.expvar()['snapshot']['upgrade_ok'], 1)
+
+    # Check that each node has the right data.
+    for n in self.cluster.nodes:
+      self.poll_query(n, 'SELECT COUNT(*) FROM foo', d_("{'results': [{'values': [[20]], 'types': ['integer'], 'columns': ['COUNT(*)']}]}"))
+
+    # Check that writes work OK post upgrade and subsequent snapshots.
+    for i in range(100):
+      l = self.cluster.wait_for_leader()
+      l.execute('INSERT INTO foo(name) VALUES("fiona")')
+
+    # Check that each node has the right data.
+    for n in self.cluster.nodes:
+      self.poll_query(n, 'SELECT COUNT(*) FROM foo', d_("{'results': [{'values': [[120]], 'types': ['integer'], 'columns': ['COUNT(*)']}]}"))
+
+  def tearDown(self):
+    self.cluster.deprovision()
+
 if __name__ == "__main__":
   unittest.main(verbosity=2)
