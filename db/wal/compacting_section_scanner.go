750d336bc154195ec108f6a94f2e384142944f52
Philip O'Toole <philip.otoole@yahoo.com>
2026-03-16 20:56:26 -0400

    DRY it up

--- db/wal/compacting_section_scanner.go
+++ db/wal/compacting_section_scanner.go
@@ -1,7 +1,6 @@
 package wal
 
 import (
-	"encoding/binary"
 	"fmt"
 	"io"
 	"maps"
@@ -15,11 +14,11 @@ import (
 // since the WAL file is trusted; the Writer recomputes checksums when
 // producing output.
 type CompactingSectionScanner struct {
-	r        io.ReaderAt
-	header   *WALHeader
-	pageSize uint32
-	start    int64
-	end      int64
+	readSeeker io.ReadSeeker
+	walReader  *Reader
+	header     *WALHeader
+	start      int64
+	end        int64
 
 	frames cFrames
 	fIdx   int
@@ -29,34 +28,13 @@ type CompactingSectionScanner struct {
 // frames from the byte range [start, end) of the WAL accessible via r. The
 // start and end offsets must be aligned to frame boundaries. The WAL header
 // is always read from offset 0.
-func NewCompactingSectionScanner(r io.ReaderAt, start, end int64) (*CompactingSectionScanner, error) {
-	hdr := make([]byte, WALHeaderSize)
-	if _, err := r.ReadAt(hdr, 0); err != nil {
-		return nil, fmt.Errorf("read WAL header: %w", err)
+func NewCompactingSectionScanner(r io.ReadSeeker, start, end int64) (*CompactingSectionScanner, error) {
+	walReader := NewReader(r)
+	if err := walReader.ReadHeader(); err != nil {
+		return nil, err
 	}
 
-	magic := binary.BigEndian.Uint32(hdr[0:])
-	var bo binary.ByteOrder
-	switch magic {
-	case 0x377f0682:
-		bo = binary.LittleEndian
-	case 0x377f0683:
-		bo = binary.BigEndian
-	default:
-		return nil, fmt.Errorf("invalid wal header magic: %x", magic)
-	}
-
-	if version := binary.BigEndian.Uint32(hdr[4:]); version != WALSupportedVersion {
-		return nil, fmt.Errorf("unsupported wal version: %d", version)
-	}
-
-	chksum1 := binary.BigEndian.Uint32(hdr[24:])
-	chksum2 := binary.BigEndian.Uint32(hdr[28:])
-	if v0, v1 := WALChecksum(bo, 0, 0, hdr[:24]); v0 != chksum1 || v1 != chksum2 {
-		return nil, fmt.Errorf("WAL header checksum mismatch")
-	}
-
-	pageSize := binary.BigEndian.Uint32(hdr[8:])
+	pageSize := walReader.pageSize
 	frameSize := int64(WALFrameHeaderSize) + int64(pageSize)
 
 	if start > end {
@@ -72,20 +50,20 @@ func NewCompactingSectionScanner(r io.ReaderAt, start, end int64) (*CompactingSe
 	}
 
 	s := &CompactingSectionScanner{
-		r: r,
+		readSeeker: r,
+		walReader:  walReader,
 		header: &WALHeader{
-			Magic:     magic,
+			Magic:     walReader.magic,
 			Version:   WALSupportedVersion,
 			PageSize:  pageSize,
-			Seq:       binary.BigEndian.Uint32(hdr[12:]),
-			Salt1:     binary.BigEndian.Uint32(hdr[16:]),
-			Salt2:     binary.BigEndian.Uint32(hdr[20:]),
-			Checksum1: chksum1,
-			Checksum2: chksum2,
+			Seq:       walReader.seq,
+			Salt1:     walReader.salt1,
+			Salt2:     walReader.salt2,
+			Checksum1: walReader.chksum1,
+			Checksum2: walReader.chksum2,
 		},
-		pageSize: pageSize,
-		start:    start,
-		end:      end,
+		start: start,
+		end:   end,
 	}
 
 	if err := s.scan(); err != nil {
@@ -107,9 +85,12 @@ func (s *CompactingSectionScanner) Next() (*Frame, error) {
 	}
 
 	cf := s.frames[s.fIdx]
-	data := make([]byte, s.pageSize)
-	if _, err := s.r.ReadAt(data, cf.Offset+WALFrameHeaderSize); err != nil {
-		return nil, fmt.Errorf("read frame data at offset %d: %w", cf.Offset+WALFrameHeaderSize, err)
+	data := make([]byte, s.header.PageSize)
+	if _, err := s.readSeeker.Seek(cf.Offset+WALFrameHeaderSize, io.SeekStart); err != nil {
+		return nil, err
+	}
+	if _, err := io.ReadFull(s.readSeeker, data); err != nil {
+		return nil, err
 	}
 	s.fIdx++
 
@@ -133,20 +114,23 @@ func (s *CompactingSectionScanner) scan() error {
 		return nil
 	}
 
-	frmHdr := make([]byte, WALFrameHeaderSize)
-	frameSize := int64(WALFrameHeaderSize) + int64(s.pageSize)
+	// Seek to the start of the section.
+	if _, err := s.readSeeker.Seek(s.start, io.SeekStart); err != nil {
+		return fmt.Errorf("seek to start offset %d: %w", s.start, err)
+	}
 
 	waitingForCommit := false
 	txFrames := make(map[uint32]*cFrame)
 	frames := make(map[uint32]*cFrame)
+	frameSize := int64(WALFrameHeaderSize) + int64(s.header.PageSize)
 
 	for offset := s.start; offset < s.end; offset += frameSize {
-		if _, err := s.r.ReadAt(frmHdr, offset); err != nil {
-			return fmt.Errorf("read frame header at offset %d: %w", offset, err)
+		pgno, commit, err := s.walReader.ReadFrame(nil)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
 		}
-
-		pgno := binary.BigEndian.Uint32(frmHdr[0:])
-		commit := binary.BigEndian.Uint32(frmHdr[4:])
 
 		txFrames[pgno] = &cFrame{
 			Pgno:   pgno,
