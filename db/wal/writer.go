d87078f585ec7befe2684b3727f6cacccc7f43fe
Philip O'Toole <philip.otoole@yahoo.com>
2026-04-07 07:21:59 -0400

    Reuse WAL frame header during writing

--- db/wal/writer.go
+++ db/wal/writer.go
@@ -54,6 +54,7 @@ type Writer struct {
 	rHeader          *WALHeader
 	chksum1, chksum2 uint32
 	bo               binary.ByteOrder
+	frmHdr           [WALFrameHeaderSize]byte
 }
 
 // NewWriter returns a new Writer.
@@ -137,7 +138,7 @@ func (w *Writer) writeWALHeader(ww io.Writer) (n int64, err error) {
 }
 
 func (w *Writer) writeFrame(ww io.Writer, frame *Frame) (n int64, err error) {
-	frmHdr := make([]byte, WALFrameHeaderSize)
+	frmHdr := w.frmHdr[:]
 
 	// Calculate the frame header.
 	binary.BigEndian.PutUint32(frmHdr[0:], frame.Pgno)
@@ -146,10 +147,16 @@ func (w *Writer) writeFrame(ww io.Writer, frame *Frame) (n int64, err error) {
 	binary.BigEndian.PutUint32(frmHdr[12:], w.rHeader.Salt2)
 
 	// Checksum of frame header: "...the first 8 bytes..."
-	w.chksum1, w.chksum2 = WALChecksum(w.bo, w.chksum1, w.chksum2, frmHdr[:8])
+	w.chksum1, w.chksum2, err = WALChecksum(w.bo, w.chksum1, w.chksum2, frmHdr[:8])
+	if err != nil {
+		return n, fmt.Errorf("error computing frame header checksum: %s", err)
+	}
 
 	// Update checksum using frame data: "..the content of all frames up to and including the current frame."
-	w.chksum1, w.chksum2 = WALChecksum(w.bo, w.chksum1, w.chksum2, frame.Data)
+	w.chksum1, w.chksum2, err = WALChecksum(w.bo, w.chksum1, w.chksum2, frame.Data)
+	if err != nil {
+		return n, fmt.Errorf("error computing frame data checksum: %s", err)
+	}
 	binary.BigEndian.PutUint32(frmHdr[16:], w.chksum1)
 	binary.BigEndian.PutUint32(frmHdr[20:], w.chksum2)
 
