5404cbbf0c932cdc73b55b919267fa8f3c7b0cc0
Philip O'Toole <philip.otoole@yahoo.com>
2026-04-27 21:38:52 -0400

    Fix release note script

--- .github/workflows/release-notes.py
+++ .github/workflows/release-notes.py
@@ -36,7 +36,7 @@ sh install.sh
 To install a specific version:
 ```
 curl -fsSL https://rqlite.io/install.sh -o install.sh
-RQLITE_VERSION={release} INSTALL_DIR={location} sh install.sh
+RQLITE_VERSION={release} INSTALL_DIR={{location}} sh install.sh
 ```
 
 ### Packages
