eb76c0535c55734713616691ed3180b47a8debda
Philip O'Toole <philip.otoole@yahoo.com>
2026-05-19 13:34:59 -0400

    Correctly pass a nil credential store

--- cmd/rqlited/main.go
+++ cmd/rqlited/main.go
@@ -437,7 +437,11 @@ func createDiscoService(cfg *Config, str *store.Store) (*disco.Service, error) {
 
 func startHTTPService(cfg *Config, str *store.Store, cltr *cluster.Client, credStr *auth.CredentialsStore, pxy *proxy.Proxy) (*httpd.Service, error) {
 	// Create HTTP server and load authentication information.
-	s := httpd.New(cfg.HTTPAddr, str, cltr, pxy, credStr)
+	var cs httpd.CredentialStore
+	if credStr != nil {
+		cs = credStr
+	}
+	s := httpd.New(cfg.HTTPAddr, str, cltr, pxy, cs)
 
 	s.CACertFile = cfg.HTTPx509CACert
 	s.CertFile = cfg.HTTPx509Cert
